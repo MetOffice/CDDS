f08630d50148324f443b9b208b1a37860a258c46
mo-jareddrayton <jared.drayton@metoffice.gov.uk>
2022-04-26 16:38:51 +0100

    Modify versions.py for use with git rather than svn.

--- mip_convert/mip_convert/versions.py
+++ mip_convert/mip_convert/versions.py
@@ -1,10 +1,9 @@
-# (C) British Crown Copyright 2016-2021, Met Office.
+# (C) British Crown Copyright 2016-2022, Met Office.
 # Please see LICENSE.rst for license details.
 """
 The :mod:`versions` module contains the code required to determine the
 version number of a package.
 """
-import os
 import subprocess
 
 
@@ -12,13 +11,14 @@ def get_version(package):
     """
     Return the version of a package.
 
-    In development mode, ``-DEV.<development_line>@r<revision>`` is
-    appended to the version, where ``<development_line>`` is the name
-    of the development line, e.g., ``trunk``, ``my_branch_name``, and
-    ``<revision>`` is the latest revision number of the repository.
+    In development mode, ``.dev0+<branch_name>.g<git_hash>`` is
+    appended to the version, where ``<branch_name>`` is the name
+    of the branch checked out, e.g., ``CDDSO-1_my-branch-name``, and
+    ``<git_hash>`` is the short hash of HEAD.
+    If any git tracked files are modified an ``M`` is appended.
     Since it is assumed that a working copy will exist in development
     mode, this information is determined directly from the working copy
-    using ``svn info`` and ``svnversion``.
+    using git commands.
 
     Parameters
     ----------
@@ -44,63 +44,63 @@ def get_version(package):
         version = getattr(imported_package, numerical_version)
     if hasattr(imported_package, dev):
         if getattr(imported_package, dev):
-            development_info = _get_development_info(imported_package.__path__)
+            development_info = _get_development_info()
             version = '{version}{development_info}'.format(
                 version=version, development_info=development_info)
     return version
 
 
-def _get_development_info(package_path):
+def _get_development_info():
     # '.devN' should be used in development mode, see PEP 440
     # (https://www.python.org/dev/peps/pep-0440/).
     development_info = '.dev0'
-    development_line = _get_development_line(package_path)
+    
+    development_line = _get_branch_name()
     if development_line:
         development_info += development_line
-    svn_revision = _get_svn_revision(package_path)
+    svn_revision = _get_git_short_hash()
     if svn_revision:
         development_info += svn_revision
+    modified = _is_modified()
+    if modified:
+        development_info += modified
+    
     return development_info
 
 
-def _get_development_line(package_path):
-    development_line = None
-    command = ['svn', 'info']
-    # Change to the top level package directory so that the URL
-    # retrieved below is always to the same directory.
-    orig_working_dir = os.getcwd()
-    os.chdir(package_path[0])
+def _get_branch_name():
+    branch_name = None
+    command = ['git', 'branch', '--show-current']
     process = subprocess.Popen(command, stdout=subprocess.PIPE,
                                stderr=subprocess.PIPE, universal_newlines=True)
-    os.chdir(orig_working_dir)
-    # The communicate() method returns a tuple in the form
-    # (stdoutdata, stderrdata).
-    svn_info_output = process.communicate()[0]
+    # The communicate() method returns a tuple in the form (stdoutdata, stderrdata).
+    git_command_output = process.communicate()[0].strip()
     if process.returncode == 0:
-        for line in svn_info_output.split('\n'):
-            if line.startswith('URL'):
-                url = line.split(': ')[1]
-                # The url ends with
-                # '/<development_line>/<package>/<package>'.
-                # Local version identifiers MUST use '+' (PEP 440).
-                development_line = '+{}'.format(url.split('/')[-3])
-    return development_line
+        branch_name = '+{}'.format(git_command_output)
+    
+    return branch_name
 
 
-def _get_svn_revision(package_path):
-    svn_revision = None
-    command = ['svnversion', '--no-newline']
-    # Change to the top level package directory so that the URL
-    # retrieved below is always to the same directory.
-    orig_working_dir = os.getcwd()
-    os.chdir(package_path[0])
+def _get_git_short_hash():
+    git_short_hash = None
+    command = ['git', 'rev-parse', '--short', 'HEAD']
     process = subprocess.Popen(command, stdout=subprocess.PIPE,
                                stderr=subprocess.PIPE, universal_newlines=True)
-    os.chdir(orig_working_dir)
-    # The communicate() method returns a tuple in the form
-    # (stdoutdata, stderrdata).
-    svnversion_output = process.communicate()[0]
+    # The communicate() method returns a tuple in the form (stdoutdata, stderrdata).
+    git_command_output = process.communicate()[0].strip()
     if process.returncode == 0:
-        if svnversion_output != 'Unversioned directory':
-            svn_revision = '.r{}'.format(svnversion_output)
-    return svn_revision
+        git_short_hash = '.g{}'.format(git_command_output)
+    
+    return git_short_hash
+
+def _is_modified():
+    modified = None
+    command = ['git', 'status', '-uno', '--short']
+    process = subprocess.Popen(command, stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE, universal_newlines=True)
+    # The communicate() method returns a tuple in the form (stdoutdata, stderrdata).
+    git_command_output = process.communicate()[0]
+    if git_command_output:
+        modified = 'M'
+    
+    return modified
