fe637e760fce7d1e4eb363fc5ff3c83235aec511
mo-kerstinschmatzer <kschmatz@vld733.cmpd1.metoffice.gov.uk>
2022-06-15 11:54:13 +0100

    CDDSO-117 Address comments from code review

--- transfer/transfer/tests/functional/__init__.py
+++ transfer/transfer/tests/functional/__init__.py
@@ -1,2 +1,5 @@
 # (C) British Crown Copyright 2022, Met Office.
 # Please see LICENSE.rst for license details.
+"""
+Functional tests for cdds_store. Each functional test is a separate module for clarity.
+"""
