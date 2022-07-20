7a601985b00c402508dbd53052394e97ab7c3222
mo-jareddrayton <jared.drayton@metoffice.gov.uk>
2022-07-20 11:13:33 +0100

    CDDSO-141: Fix failing test.

--- cdds/cdds/tests/test_plugins/test_plugin_loader.py
+++ cdds/cdds/tests/test_plugins/test_plugin_loader.py
@@ -14,6 +14,9 @@ from cdds.tests.test_plugins.stubs import EmptyCddsPlugin
 
 class TestLoadCmipPlugin(TestCase):
 
+    def setUp(self):
+        PluginStore.clean_instance()
+
     def tearDown(self):
         PluginStore.clean_instance()
 
@@ -29,6 +32,7 @@ class TestLoadCmipPlugin(TestCase):
 class TestLoadPlugin(TestCase):
 
     def setUp(self):
+        PluginStore.clean_instance()
         self.plugin_store = PluginStore.instance()
 
     def tearDown(self):
