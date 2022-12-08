25c9c8cd4c26f0bd284b3bf3c9d00a723925a67f
mo-jareddrayton <jared.drayton@metoffice.gov.uk>
2022-12-08 14:21:38 +0000

    Add HadGEM3_GC31_MH plugin

--- cdds/cdds/common/plugins/cmip6/cmip6_models.py
+++ cdds/cdds/common/plugins/cmip6/cmip6_models.py
@@ -31,6 +31,7 @@ class Cmip6ModelId(ModelId):
     HadGEM3_GC31_LL = 'HadGEM3-GC31-LL'
     HadGEM3_GC31_MM = 'HadGEM3-GC31-MM'
     HadGEM3_GC31_HM = 'HadGEM3-GC31-HM'
+    HadGEM3_GC31_MH = 'HadGEM3-GC31-MH'
     UKESM1_0_LL = 'UKESM1-0-LL'
     UKESM1_1_LL = 'UKESM1-1-LL'
     UKESM1_ICE_LL = 'UKESM1-ice-LL'
@@ -195,6 +196,45 @@ class HadGEM3_GC31_HM_Params(BaseModelParameters):
         return '10.7'
 
 
+class HadGEM3_GC31_MH_Params(BaseModelParameters):
+    """
+    Class to store the parameters for the HadGEM3_GC31_MH model.
+    """
+
+    def __init__(self) -> None:
+        super(HadGEM3_GC31_MH_Params, self).__init__(Cmip6ModelId.HadGEM3_GC31_MH)
+
+    @property
+    def model_version(self) -> str:
+        """
+        Returns the model version of the HadGEM3_GC31_MH model.
+
+        :return: Model version of HadGEM3_GC31_MH
+        :rtype: str
+        """
+        return '3.1'
+
+    @property
+    def data_request_version(self) -> str:
+        """
+        Returns the data request version of the HadGEM3_GC31_MH model.
+
+        :return: Data request version of HadGEM3_GC31_MH
+        :rtype: str
+        """
+        return '01.00.10'
+
+    @property
+    def um_version(self) -> str:
+        """
+        Returns the UM version of the HadGEM3_GC31_MH model.
+
+        :return: UM version of HadGEM3_GC31_MH
+        :rtype: str
+        """
+        return '10.7'
+
+
 class UKESM1_0_LL_Params(BaseModelParameters):
     """
     Class to store the parameters for the UKESM1_0_LL model.
@@ -327,6 +367,7 @@ class Cmip6ModelsStore(BaseModelStore):
             HadGEM3_GC31_LL_Params(),
             HadGEM3_GC31_MM_Params(),
             HadGEM3_GC31_HM_Params(),
+            HadGEM3_GC31_MH_Params(),
             UKESM1_0_LL_Params(),
             UKESM1_1_LL_Params(),
             UKESM1_ice_LL_Params()
