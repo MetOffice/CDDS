99120f7dc02b15318e35a6712ca81461d675be36
pflorek <piotr.florek@metoffice.gov.uk>
2023-02-08 09:55:43 +0000

    CDDSO-172 fixing unit tests and removing conflict markup

--- mip_convert/mip_convert/tests/test_functional/test_functional_seasonal/test_seasonal_omon_tos_no_mask.py
+++ mip_convert/mip_convert/tests/test_functional/test_functional_seasonal/test_seasonal_omon_tos_no_mask.py
@@ -2,7 +2,7 @@
 # Please see LICENSE.rst for license details.
 import os
 
-from nose.plugins.attrib import attr
+import pytest
 
 from mip_convert.tests.test_functional.test_command_line import AbstractFunctionalTests
 from mip_convert.tests.test_functional.utils.configurations import SeasonalTestData, SpecificInfo
@@ -48,6 +48,6 @@ class TestSeasonalOmonTosNoMask(AbstractFunctionalTests):
             )
         )
 
-    @attr('slow')
+    @pytest.mark.slow
     def test_seasonal_omon_tos_no_mask(self):
         self.check_convert()
