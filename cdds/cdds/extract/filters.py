20cbcb8abe03698c2aba4c971f2cdbe4c6e59dc1
mo-kerstinschmatzer <kerstin.schmatzer@metoffice.gov.uk>
2023-05-24 09:42:27 +0000

    CDDSO-279 Remove print statement

--- cdds/cdds/extract/filters.py
+++ cdds/cdds/extract/filters.py
@@ -520,7 +520,6 @@ class Filters(object):
         self.mass_cmd = []
         error = ""
         start_date = calculate_period((start.year, start.month, start.day), True)
-        print('Start date of period: {}'.format(start_date))
         end_date = calculate_period((end.year, end.month, end.day), False)
         test_sizes = self._chunk_candidates(start_date, end_date)
         chunks, with_ens_id = self.generate_chunks(start_date, end, test_sizes)
