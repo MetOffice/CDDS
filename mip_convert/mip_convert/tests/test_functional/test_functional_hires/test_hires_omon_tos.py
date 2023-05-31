298e67138fdb6b0d41740a07189feb9a86c192f1
pflorek <piotr.florek@metoffice.gov.uk>
2023-05-31 10:14:47 +0100

    CDDSO-153 added orca12 unit tests

---
+++ mip_convert/mip_convert/tests/test_functional/test_functional_hires/test_hires_omon_tos.py
@@ -0,0 +1,49 @@
+# (C) British Crown Copyright 2023, Met Office.
+# Please see LICENSE.rst for license details.
+import os
+
+import pytest
+
+from mip_convert.tests.test_functional.test_command_line import AbstractFunctionalTests
+from mip_convert.tests.test_functional.utils.configurations import HiResTestData, SpecificInfo
+from mip_convert.tests.test_functional.utils.directories import (get_cmor_log, get_output_dir,
+                                                                 MODEL_OUTPUT_DIR_SET1,
+                                                                 ROOT_TEST_CASES_DIR,
+                                                                 ROOT_ANCIL_DIR)
+
+
+class TestHiResOmonTos(AbstractFunctionalTests):
+
+    def get_test_data(self):
+        test_location = os.path.join(ROOT_TEST_CASES_DIR, 'test_HIRES_Omon_tos')
+        return HiResTestData(
+            mip_table='Omon',
+            variable='tos',
+            specific_info=SpecificInfo(
+                common={
+                    'test_location': test_location
+                },
+                cmor_setup={
+                    'cmor_log_file': get_cmor_log(test_location)
+                },
+                cmor_dataset={
+                    'output_dir': get_output_dir(test_location),
+                },
+                request={
+                    'ancil_files': os.path.join(ROOT_ANCIL_DIR, 'HadGEM3-GC31-HH', 'ocean_byte_masks_HH.nc'),
+                    'model_output_dir': MODEL_OUTPUT_DIR_SET1,
+                    'run_bounds': '1951-01-01-00-00-00 1951-02-01-00-00-00',
+                    'suite_id': 'u-ay652'
+                },
+                streams={
+                    'onm': {'CMIP6_Omon': 'tos'}
+                },
+                other={
+                    'filenames': ['tos_Omon_HadGEM3-GC31-HH_hist-1950_r1i1p1f1_gn_195101-195101.nc'],
+                }
+            )
+        )
+
+    @pytest.mark.superslow
+    def test_hires_omon_tos_superslow(self):
+        self.check_convert()
