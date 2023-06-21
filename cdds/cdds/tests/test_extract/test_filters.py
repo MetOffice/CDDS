4b66db17d6436a690c31b3a452f2dfd8c778bc97
piotr-florek-mohc <34739314+piotr-florek-mohc@users.noreply.github.com>
2023-06-21 09:18:42 +0100

    CDDSO-289 modified chunking logic and unit tests (#133)
    * CDDSO-289 modified chunking logic and unit tests
    
    * CDDSO-289 undo file limit change
    
    * CDDSO-289 logging chunk size finding failure

--- cdds/cdds/tests/test_extract/test_filters.py
+++ cdds/cdds/tests/test_extract/test_filters.py
@@ -389,32 +389,32 @@ class TestFilters(unittest.TestCase):
             filters.filters
         )
 
-    def test_generate_chunks(self):
+    @patch("cdds.extract.filters.Filters._test_chunks")
+    def test_generate_chunks(self, mock_test_chunks):
         filters = Filters(var_list=self.VAR_LIST)
-        chunks = filters.generate_chunks((1900, 1, 1),
-                                         datetime.datetime(2000, 1, 1), 50)
+        mock_test_chunks.return_value = 50
+        chunks, _ = filters.generate_chunks((1900, 1, 1), datetime.datetime(2000, 1, 1), [50])
         self.assertEqual(chunks, [
             {'start': (1900, 1, 1), 'end': (1949, 12, 21)},
             {'start': (1950, 1, 1), 'end': (1999, 12, 21)}
         ])
-        chunks = filters.generate_chunks((1900, 1, 1),
-                                         datetime.datetime(2000, 1, 1), 100)
+        mock_test_chunks.return_value = 100
+        chunks, _ = filters.generate_chunks((1900, 1, 1), datetime.datetime(2000, 1, 1), [100])
         self.assertEqual(chunks, [
             {'start': (1900, 1, 1), 'end': (1999, 12, 21)}
         ])
-        chunks = filters.generate_chunks((1900, 1, 1),
-                                         datetime.datetime(2000, 1, 1), 200)
+        mock_test_chunks.return_value = 200
+        chunks, _ = filters.generate_chunks((1900, 1, 1), datetime.datetime(2000, 1, 1), [200])
         self.assertEqual(chunks, [
             {'start': (1900, 1, 1), 'end': (1999, 12, 21)}
         ])
-        chunks = filters.generate_chunks((1800, 1, 1),
-                                         datetime.datetime(1850, 1, 1), 50)
+        mock_test_chunks.return_value = 50
+        chunks, _ = filters.generate_chunks((1800, 1, 1), datetime.datetime(1850, 1, 1), [50])
         self.assertEqual(chunks, [
             {'start': (1800, 1, 1), 'end': (1849, 12, 21)}
         ])
-
-        chunks = filters.generate_chunks((1850, 1, 1),
-                                         datetime.datetime(1852, 1, 1), 0.5)
+        mock_test_chunks.return_value = 0.5
+        chunks, _ = filters.generate_chunks((1850, 1, 1), datetime.datetime(1852, 1, 1), [0.5])
         expected = [
             {'start': (1850, 1, 1), 'end': (1850, 7, 1)},
             {'start': (1850, 7, 1), 'end': (1850, 12, 21)},
@@ -422,9 +422,8 @@ class TestFilters(unittest.TestCase):
             {'start': (1851, 7, 1), 'end': (1851, 12, 21)},
         ]
         self.assertEqual(chunks, expected)
-
-        chunks = filters.generate_chunks((1850, 1, 1),
-                                         datetime.datetime(1851, 1, 1), 0.25)
+        mock_test_chunks.return_value = 0.25
+        chunks, _ = filters.generate_chunks((1850, 1, 1), datetime.datetime(1851, 1, 1), [0.25])
         expected = [
             {'start': (1850, 1, 1), 'end': (1850, 4, 1)},
             {'start': (1850, 4, 1), 'end': (1850, 7, 1)},
