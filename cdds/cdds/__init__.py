4b8bbc5cc06f326c7a2fd3a9163ccb44b5efe77d
Matthew Mizielinski <matthew.mizielinski@metoffice.gov.uk>
2023-07-27 12:13:45 +0000

    CDDSO-112: Update copyrights (and rebuild docs again)

--- cdds/cdds/__init__.py
+++ cdds/cdds/__init__.py
@@ -1,4 +1,4 @@
-# (C) British Crown Copyright 2022, Met Office.
+# (C) British Crown Copyright 2022-2023, Met Office.
 # Please see LICENSE.rst for license details.
 from cdds.versions import get_version
 
