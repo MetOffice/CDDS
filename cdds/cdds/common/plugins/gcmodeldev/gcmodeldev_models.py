b438b24d4729fe3be78c54d7390675a49090d5ed
mo-kerstinschmatzer <kerstin.schmatzer@metoffice.gov.uk>
2023-10-12 15:00:50 +0000

    CDDSO-354 Add model to plugins and mappings

--- cdds/cdds/common/plugins/gcmodeldev/gcmodeldev_models.py
+++ cdds/cdds/common/plugins/gcmodeldev/gcmodeldev_models.py
@@ -29,6 +29,7 @@ class GCModelDevStore(BaseModelStore):
             HadGEM3_GC31_MM_Params(),
             HadGEM3_GC3p05_N216ORCA025_Params(),
             HadGEM3_GC5p_N216ORCA025_Params(),
+            HadREM3_CP4A_2p2km_Params(),
             UKESM1_0_LL_Params(),
             UKESM1_1_LL_Params()
         ]
@@ -76,6 +77,7 @@ class GCModelDevModelId(ModelId):
         return '{}.json'.format(self.value)
 
     HadREM3_GA7_05 = 'HadREM3-GA7-05'
+    HadREM3_CP4A_2p2km = 'HadREM3-CP4A-2p2km'
     HadGEM3_GC31_LL = 'HadGEM3-GC31-LL'
     HadGEM3_GC31_MM = 'HadGEM3-GC31-MM'
     HadGEM3_GC3p05_N216ORCA025 = 'HadGEM3-GC3p05-N216ORCA025'
@@ -83,6 +85,45 @@ class GCModelDevModelId(ModelId):
     UKESM1_0_LL = 'UKESM1-0-LL'
     UKESM1_1_LL = 'UKESM1-1-LL'
     UKESM1_ICE_LL = 'UKESM1-ice-LL'
+
+
+class HadREM3_CP4A_2p2km_Params(BaseModelParameters):
+    """
+    Class to store the parameters fo the HadREM3_CP4A_2p2km model.
+    """
+
+    def __init__(self):
+        super(HadREM3_CP4A_2p2km_Params, self).__init__(GCModelDevModelId.HadREM3_CP4A_2p2km)
+
+    @property
+    def model_version(self) -> str:
+        """
+        Returns the model version of the HadREM3_CP4A_2p2km model.
+
+        :return: Model version of HadREM3_CP4A_2p2km
+        :rtype: str
+        """
+        return '4.0'
+
+    @property
+    def data_request_version(self) -> str:
+        """
+        Returns the data request version of the HadREM3_CP4A_2p2km model.
+
+        :return: Data request version of HadREM3_CP4A_2p2km
+        :rtype: str
+        """
+        return ''
+
+    @property
+    def um_version(self) -> str:
+        """
+        Returns the UM version of the HadREM3_CP4A_2p2km model.
+
+        :return: UM version of HadREM3_CP4A_2p2km
+        :rtype: str
+        """
+        return '10.9'
 
 
 class HadGEM3_GC3p05_N216ORCA025_Params(BaseModelParameters):
