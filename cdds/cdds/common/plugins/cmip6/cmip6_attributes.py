5a7453558208bdae1b4c4bf6b3304f82c6aed519
mo-kerstinschmatzer <kerstin.schmatzer@metoffice.gov.uk>
2024-02-29 09:32:36 +0000

    CDDSO-411 Update copyright

--- cdds/cdds/common/plugins/cmip6/cmip6_attributes.py
+++ cdds/cdds/common/plugins/cmip6/cmip6_attributes.py
@@ -1,4 +1,4 @@
-# (C) British Crown Copyright 2022-2023, Met Office.
+# (C) British Crown Copyright 2022-2024, Met Office.
 # Please see LICENSE.rst for license details.
 from cdds.common.plugins.attributes import GlobalAttributes
 from typing import TYPE_CHECKING
--- cdds/cdds/common/plugins/cmip6_plus/cmip6_plus_attributes.py
+++ cdds/cdds/common/plugins/cmip6_plus/cmip6_plus_attributes.py
@@ -1,4 +1,4 @@
-# (C) British Crown Copyright 2023, Met Office.
+# (C) British Crown Copyright 2024, Met Office.
 # Please see LICENSE.rst for license details.
 """
 The :mod:`cmip6_plus_attributes` module contains the code required to
