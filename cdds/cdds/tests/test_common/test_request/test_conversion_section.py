8271bca51cf58aa0563a3ef5033b1860d6426747
Matthew Mizielinski <matthew.mizielinski@metoffice.gov.uk>
2024-04-19 08:28:06 +0000

    CDDSO-452: Extend test coverage

--- cdds/cdds/tests/test_common/test_request/test_conversion_section.py
+++ cdds/cdds/tests/test_common/test_request/test_conversion_section.py
@@ -29,8 +29,29 @@ class TestConversionDefaults(TestCase):
 
         self.assertDictEqual(defaults, expected_defaults)
 
+    @mock.patch.dict(os.environ, {'CDDS_CONVERT_WORKFLOW_BRANCH': 'mocked'})
     @mock.patch('cdds.common.request.conversion_section.whereami')
     def test_defaults_for_metoffice(self, whereami_mock):
+        whereami_mock.return_value = Facility.MET_OFFICE
+        expected_defaults = {
+            'cdds_workflow_branch': 'mocked',
+            'cylc_args': '-v',
+            'no_email_notifications': True,
+            'skip_extract': False,
+            'skip_extract_validation': False,
+            'skip_configure': False,
+            'skip_qc': False,
+            'skip_archive': False,
+            'continue_if_mip_convert_failed': False
+        }
+
+        defaults = conversion_defaults()
+
+        self.assertDictEqual(defaults, expected_defaults)
+
+    @mock.patch.dict(os.environ, {})
+    @mock.patch('cdds.common.request.conversion_section.whereami')
+    def test_defaults_for_metoffice_env_var_unset(self, whereami_mock):
         whereami_mock.return_value = Facility.MET_OFFICE
         expected_defaults = {
             'cdds_workflow_branch': 'trunk',
