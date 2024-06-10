14a0d56f75da6feb46d31e910295752ef69e3eae
mo-kerstinschmatzer <kerstin.schmatzer@metoffice.gov.uk>
2024-06-10 10:34:01 +0000

    CDDSO-450 cdds_clean script (#296)
    Cherry-pick from release branch into main
    
    * Clean cdds stream workflows
    * Make cdds_clean executable

---
+++ cdds/cdds/clean/command_line.py
@@ -0,0 +1,60 @@
+# (C) British Crown Copyright 2024, Met Office.
+# Please see LICENSE.rst for license details.
+"""
+Module containing functionality to clean CDDS stream workflows
+"""
+import logging
+
+from cdds.common import configure_logger
+from cdds.common.request.request import read_request
+from cdds.clean.workflows import clean_workflows
+
+from argparse import Namespace, ArgumentParser, ArgumentDefaultsHelpFormatter
+from typing import List
+
+
+CDDS_CLEAN_LOG = 'cdds_clean'
+
+
+def run_cdds_clean(arguments: List[str] = None) -> int:
+    """
+    Clean the CDDS stream workflows
+
+    :param arguments: Command line arguments
+    :type arguments: List[str]
+    :return: Exit code
+    :rtype: int
+    """
+    args = parse_arguments(arguments)
+    request = read_request(args.request)
+
+    configure_logger(CDDS_CLEAN_LOG, logging.INFO, False)
+    logger = logging.getLogger(__name__)
+
+    try:
+        clean_workflows(request)
+        return 0
+    except BaseException as exc:
+        logger.exception(exc, exc_info=1)
+        return 1
+
+
+def parse_arguments(arguments: List[str]) -> Namespace:
+    """
+    Parse command line arguments for running CDDS clean
+
+    :param arguments: Command line arguments to parse
+    :type arguments: List[str]
+    :return: Parsed command line arguments
+    :rtype: Namespace
+    """
+    parser = ArgumentParser(
+        description='Clean the CDDS workflows.',
+        formatter_class=ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        'request', help=(
+            'The full path to the configuration file containing information about the request.')
+    )
+
+    return parser.parse_args(arguments)
---
+++ cdds/cdds/tests/test_clean/test_workflows.py
@@ -0,0 +1,81 @@
+# (C) British Crown Copyright 2024, Met Office.
+# Please see LICENSE.rst for license details.
+import logging
+import os
+
+from unittest import TestCase, mock
+
+from cdds.clean.workflows import clean_workflows
+from cdds.common.request.request import Request
+
+
+class TestCleanWorkflows(TestCase):
+
+    def setUp(self):
+        logging.disable(logging.CRITICAL)
+
+    @mock.patch('cdds.clean.workflows.run_command')
+    def test_clean_workflow_for_one_stream(self, mock_run_command):
+        mock_run_command.return_value = '8'
+        ap6_workflow = 'cdds_workflow_ap6'
+
+        request = Request()
+        request.common.workflow_basename = 'workflow'
+        request.data.streams = ['ap6']
+        request.conversion.cylc_args = ['--workflow-name=cdds_{request_id}_{stream}']
+
+        clean_workflows(request)
+
+        calls = [mock.call(['cylc', '--version']),
+                 mock.call(['cylc', 'clean', ap6_workflow])]
+
+        mock_run_command.assert_has_calls(calls)
+
+    @mock.patch('cdds.clean.workflows.run_command')
+    def test_clean_workflow_for_multiple_streams(self, mock_run_command):
+        mock_run_command.return_value = '8.2'
+        ap6_workflow = 'cdds_workflow_ap6'
+        ap5_workflow = 'cdds_workflow_ap5'
+        ap4_workflow = 'cdds_workflow_ap4'
+
+        request = Request()
+        request.common.workflow_basename = 'workflow'
+        request.data.streams = ['ap6', 'ap5', 'ap4']
+        request.conversion.cylc_args = ['--workflow-name=cdds_{request_id}_{stream}']
+
+        clean_workflows(request)
+
+        calls = [mock.call(['cylc', '--version']),
+                 mock.call(['cylc', 'clean', ap6_workflow]),
+                 mock.call(['cylc', 'clean', ap5_workflow]),
+                 mock.call(['cylc', 'clean', ap4_workflow])]
+        mock_run_command.assert_has_calls(calls)
+
+    @mock.patch('cdds.clean.workflows.run_command')
+    def test_clean_workflow_customised_workflow_name(self, mock_run_command):
+        mock_run_command.return_value = '8'
+
+        request = Request()
+        request.common.workflow_basename = 'workflow'
+        request.data.streams = ['ap6']
+        request.conversion.cylc_args = ['--workflow-name=cdds_my_workflow_{stream}']
+
+        clean_workflows(request)
+
+        calls = [mock.call(['cylc', '--version']),
+                 mock.call(['cylc', 'clean', 'cdds_my_workflow_ap6'])]
+
+        mock_run_command.assert_has_calls(calls)
+
+    @mock.patch('cdds.clean.workflows.run_command')
+    def test_clean_workflow_wrong_cylc_version(self, mock_run_command):
+        mock_run_command.return_value = '7'
+
+        request = Request()
+        request.common.workflow_basename = 'workflow'
+        request.data.streams = ['ap6', 'ap5', 'ap4']
+        request.conversion.cylc_args = ['--workflow-name=cdds_{request_id}_{stream}']
+
+        self.assertRaises(ValueError, clean_workflows, request)
+
+        mock_run_command.assert_called_once_with(['cylc', '--version'])
