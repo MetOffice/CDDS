a38bff1e0e2d25f747501884df9097657087b199
mo-kerstinschmatzer <kerstin.schmatzer@metoffice.gov.uk>
2024-10-16 13:58:34 +0100

    CDDSO-528 Update copyright

--- cdds/cdds/common/grids.py
+++ cdds/cdds/common/grids.py
@@ -1,4 +1,4 @@
-# (C) British Crown Copyright 2018-2022, Met Office.
+# (C) British Crown Copyright 2018-2024, Met Office.
 # Please see LICENSE.rst for license details.
 """
 The :mod:`grids` module contains the code required to handle
