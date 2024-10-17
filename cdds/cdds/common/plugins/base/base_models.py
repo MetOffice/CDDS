bfe006330c9403a6ff66af50d95c6d9b2c7567fa
mo-kerstinschmatzer <kerstin.schmatzer@metoffice.gov.uk>
2024-10-17 09:00:39 +0000

    CDDSO-528 Update copyright

--- cdds/cdds/common/plugins/base/base_models.py
+++ cdds/cdds/common/plugins/base/base_models.py
@@ -1,4 +1,4 @@
-# (C) British Crown Copyright 2021-2022, Met Office.
+# (C) British Crown Copyright 2021-2024, Met Office.
 # Please see LICENSE.rst for license details.
 """
 The :mod:`base_models` module contains the code required to
