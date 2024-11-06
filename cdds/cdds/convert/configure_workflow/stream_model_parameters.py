2d5a07b6f7f3038d2a69bb80f9c0fb36ad5a46e1
Jared Drayton <68954395+mo-jareddrayton@users.noreply.github.com>
2024-11-06 13:11:47 +0000

    CDDSO-538 Add unit tests for StreamModelParameters (#362)
    * CDDSO-538 Add unit tests for StreamModelParameters
    
    * CDDSO-538 Fix pep8 issues

--- cdds/cdds/convert/configure_workflow/stream_model_parameters.py
+++ cdds/cdds/convert/configure_workflow/stream_model_parameters.py
@@ -14,89 +14,96 @@ class StreamModelParameters:
     def __init__(self, request, stream, components):
         self._request = request
         self.stream = stream
-        self.components = components.stream_components
-        self.sub_streams = components.substreams_dict
+        self.stream_components = components.stream_components
+        self.stream_substreams = components.substreams_dict
         self._plugin = PluginStore.instance().get_plugin()
         self._model_params = self._plugin.models_parameters(self._request.metadata.model_id)
         self.logger = logging.getLogger()
 
     def concatenation_period(self) -> Duration:
         """
-        Calculate the maximum concatenation window size in years for this run.
+        Calculate the maximum concatenation window size in years for this model.
         This represents the longest period of data that will be stored in a
         single output file.
+
+        :return: The concatenation period to be used for the given self.stream
+        :rtype: Duration
         """
-        active_streams = [self.stream]
         model_sizing = self._model_params.full_sizing_info()
+
         if model_sizing is None or model_sizing == {}:
             max_concat_period = 10
         else:
-            # get the max file length for all data dimensions for this model
-            max_concat_period = max(max(
-                output_length for data_dims, output_length in
-                list(size_info.items())) for freq, size_info in list(model_sizing.items()))
-        max_concat_period = max_concat_period
+            concatenation_periods = []
+            for data_dims in model_sizing.values():
+                concatenation_periods += list(data_dims.values())
+            max_concat_period = max(concatenation_periods)
 
-        _concat_task_periods_years = {
-            stream: int(max_concat_period)
-            for stream in active_streams
-        }
-        _concat_task_periods_cylc = {
-            stream: 'P{years}Y'.format(years=years)
-            for stream, years
-            in _concat_task_periods_years.items()}
-
-        return DurationParser().parse(_concat_task_periods_cylc[self.stream])
+        return Duration(years=max_concat_period)
 
     def cycling_frequency(self) -> Duration:
         """
         Obtain the cycling frequency for this model and stream from the configs after applying any
         command line overrides.
-        If the default cycling frequency from the config .json has a longer duration than the run bounds,
-        a default duration of P1Y is used.
 
-        :param stream: |stream identifier| to calculate value for.
-        :type stream: str
-        :return: Cycling frequency configuration information, a string representing the number of years
-            formatted for cylc.
-        :rtype: str
+        :return: A isodatetime Duration representing the cycling frequency.
+        :rtype: Duration
         """
+        default_cycling_frequency = self._model_params.cycle_length(self.stream)
+
         cycle_overrides = {}
-        for entry in self._request.conversion.override_cycling_frequency:
-            stream, frequency = entry.split('=')
+        for override in self._request.conversion.override_cycling_frequency:
+            stream, frequency = override.split('=')
             cycle_overrides[stream] = frequency
 
-        cycle_length = self._model_params.cycle_length(self.stream)
-
         if self.stream in cycle_overrides.keys():
-            frequency = cycle_overrides[self.stream]
+            cycling_frequency = cycle_overrides[self.stream]
             self.logger.info('Overriding cycling frequency for stream "{}": "{}" -> "{}"'
-                             ''.format(stream, cycle_length, frequency))
+                             ''.format(stream, default_cycling_frequency, cycling_frequency))
             return DurationParser().parse(frequency)
         else:
-            return DurationParser().parse(cycle_length)
+            cycling_frequency = default_cycling_frequency
+
+        return DurationParser().parse(cycling_frequency)
 
     @property
     def memory(self) -> Dict[str, str]:
-        required_memory = {c: self._model_params.memory(self.stream) for c in self.components[self.stream]}
-        # Scale memory limits if included on command line
-        if self._request.conversion.scale_memory_limits:
-            required_memory = {
-                component: scale_memory(mem_limit, self._request.conversion.scale_memory_limits)
-                for component, mem_limit in required_memory.items()
-            }
+        """
+        Return the required memory for this stream and apply any scaling if a scaling factor is provided.
 
-        return required_memory
+        :return: The required memory for this stream.
+        :rtype: Dict[str, str]
+        """
+        required_memory = {c: self._model_params.memory(self.stream) for c in self.stream_components[self.stream]}
+        scaling_factor = self._request.conversion.scale_memory_limits
+
+        if scaling_factor:
+            scaled_memory = {}
+            for component, mem_limit in required_memory.items():
+                scaled_memory[component] = scale_memory(mem_limit, scaling_factor)
+            return scaled_memory
+        else:
+            return required_memory
 
     @property
     def mip_convert_temp_sizes(self) -> int:
+        """The requested temporary memory for this stream.
+
+        :return: The requested memory.
+        :rtype: int
+        """
         return self._model_params.temp_space(self.stream)
 
     def as_jinja2(self) -> Dict:
+        """A helper method that provides a dictionary with the needed jinja2 template variables.
+
+        :return: A dictionary with Jinja2 template variables.
+        :rtype: Dict
+        """
         return {
             'STREAMS': self.stream,
             'MEMORY_CONVERT': self.memory,
             'MIP_CONVERT_TMP_SPACE': self.mip_convert_temp_sizes,
-            'STREAM_COMPONENTS': self.components[self.stream],
-            'STREAM_SUBSTREAMS': self.sub_streams[self.stream],
+            'STREAM_COMPONENTS': self.stream_components[self.stream],
+            'STREAM_SUBSTREAMS': self.stream_substreams[self.stream],
         }
