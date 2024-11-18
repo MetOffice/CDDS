ac471507a7baa975ac8be17ca7661370e06ec68f
mo-kerstinschmatzer <kerstin.schmatzer@metoffice.gov.uk>
2024-11-18 14:40:33 +0000

    CDDSO-532 Fix accessing global attributes cache

--- cdds/cdds/tests/test_common/test_request/test_conversion_section.py
+++ cdds/cdds/tests/test_common/test_request/test_conversion_section.py
@@ -53,12 +53,12 @@ class TestConversionDefaults(TestCase):
 
         self.assertDictEqual(defaults, expected_defaults)
 
-    @mock.patch.dict(os.environ, {})
+    @mock.patch.dict(os.environ, {'CDDS_CONVERT_WORKFLOW_BRANCH': ''})
     @mock.patch('cdds.common.request.conversion_section.whereami')
     def test_defaults_for_metoffice_env_var_unset(self, whereami_mock):
         whereami_mock.return_value = Facility.MET_OFFICE
         expected_defaults = {
-            'cdds_workflow_branch': 'trunk',
+            'cdds_workflow_branch': '',
             'cylc_args': '-v',
             'delete_preexisting_proc_dir': False,
             'delete_preexisting_data_dir': False,
