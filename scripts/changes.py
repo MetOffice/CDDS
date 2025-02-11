cc571da7851fe8b38404ac4053311c705fa4516a
mo-jareddrayton <jared.drayton@metoffice.gov.uk>
2025-02-11 10:18:52 +0000

    CDDSO-600 Action review comments

--- scripts/changes.py
+++ scripts/changes.py
@@ -1,4 +1,4 @@
-# (C) British Crown Copyright 2024, Met Office.
+# (C) British Crown Copyright 2025, Met Office.
 # Please see LICENSE.rst for license details.
 """Generate changelog pages on the fly."""
 
@@ -8,6 +8,9 @@ import mkdocs_gen_files
 
 
 def generate_changes():
+    """ Generates a CHANGES.md page from the corresponding file in the cdds/mip_convert packages.
+    See https://oprypin.github.io/mkdocs-gen-files/index.html for API usage examples.
+    """
     packages = ["cdds", "mip_convert"]
     for package in packages:
         package_path = Path(__file__).parent.parent / package / "CHANGES.md"
@@ -17,5 +20,6 @@ def generate_changes():
             with open(package_path, "r") as fh:
                 for line in fh.readlines():
                     print(f"{line}", file=f)
+
 
 generate_changes()
