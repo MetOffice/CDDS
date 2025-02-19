df78c533a4ceaa3ad32d4f225c394288448f7c64
Kerstin Schmatzer <kerstin.schmatzer@metoffice.gov.uk>
2025-02-19 16:15:11 +0000

    CDDSO-614 Create eUKESM1 mapping plugin

---
+++ mip_convert/mip_convert/plugins/eukesm1/eukesm1_plugin.py
@@ -0,0 +1,40 @@
+# (C) British Crown Copyright 2025, Met Office.
+# Please see LICENSE.md for license details.
+"""
+The :mod:`eukesm1_plugin` module contains the code for the eUKESM1 plugin.
+"""
+import iris.cube
+import os
+
+from typing import Dict, Any
+
+from mip_convert.plugins.base.base_plugin import BaseMappingPlugin
+from mip_convert.plugins.eukesm1.data.processors import *
+
+
+class eUKESM1MappingPlugin(BaseMappingPlugin):
+    """
+    Plugin for eUKESM1 models
+    """
+
+    def __init__(self):
+        data_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
+        super(eUKESM1MappingPlugin, self).__init__('eUKESM1', data_dir)
+
+        self.input_variables: Dict[str, iris.cube.Cube] = {}
+
+    def evaluate_expression(self, expression: Any, input_variables: Dict[str, iris.cube.Cube]) -> iris.cube.Cube:
+        """
+        Update the iris Cube containing in the input variables list by evaluating the given expression.
+
+        :param expression: Expression to be evaluated
+        :type expression: Any
+        :param input_variables: The input variables required to produce the
+            MIP requested variable in the form {input_variable_name: cube}.
+        :type input_variables: Dict[str, Cube]
+        :return: The updated iris Cube
+        :rtype: Cube
+        """
+        # TODO: Remove assign to class variable after refactoring mipconvert.mipconvert.new_variable line 793
+        self.input_variables = input_variables
+        return eval(expression)
