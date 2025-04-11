1c9e4873d7bd7f3f0de040718724ead69845b108
Jared Drayton <68954395+mo-jareddrayton@users.noreply.github.com>
2025-04-11 15:03:49 +0000

    CDDSO-628 Update numerical version

--- mip_convert/mip_convert/__init__.py
+++ mip_convert/mip_convert/__init__.py
@@ -16,5 +16,5 @@ environ["VECLIB_MAXIMUM_THREADS"] = "1"
 environ["NUMEXPR_NUM_THREADS"] = "1"
 
 _DEV = False
-_NUMERICAL_VERSION = '3.1.2'
+_NUMERICAL_VERSION = '3.1.2rc1'
 __version__ = get_version('mip_convert')
