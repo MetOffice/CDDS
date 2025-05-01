30f98e227353295da4e0b1c593ce5cfa55b8753b
Jared Drayton <68954395+mo-jareddrayton@users.noreply.github.com>
2025-05-01 09:45:29 +0100

    CDDSO-628 Restore dev mode and bump version.

--- cdds/cdds/__init__.py
+++ cdds/cdds/__init__.py
@@ -2,6 +2,6 @@
 # Please see LICENSE.md for license details.
 from cdds.versions import get_version
 
-_DEV = False
-_NUMERICAL_VERSION = '3.1.2'
+_DEV = True
+_NUMERICAL_VERSION = '3.1.3'
 __version__ = get_version('cdds')
