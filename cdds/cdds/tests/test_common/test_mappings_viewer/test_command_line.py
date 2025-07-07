87d8b959b16984d0c987ea8665eb0c815572545a
mo-gill <edward.gillett@metoffice.gov.uk>
2025-07-07 15:19:32 +0000

    #474: Add a functional test.

---
+++ cdds/cdds/tests/test_common/test_mappings_viewer/test_command_line.py
@@ -0,0 +1,33 @@
+# (C) British Crown Copyright 2017-2025, Met Office.
+# Please see LICENSE.md for license details.
+# pylint: disable = missing-docstring, invalid-name, too-many-public-methods
+import unittest
+from tempfile import TemporaryDirectory
+import os
+from cdds.common.mappings_viewer.command_line import main
+from unittest.mock import patch
+import unittest
+import tempfile
+import os
+import sys
+
+
+class TestCommandLineHTMLGeneration(unittest.TestCase):
+
+    def test_html_file_created(self):
+        with tempfile.TemporaryDirectory() as tmpdir:
+
+            sys.argv = [
+                "generate_mappings_html",
+                "-s", os.path.expandvars("$CDDS_ETC/STASHmaster/vn13.0/ctldata/STASHmaster/STASHmaster-meta.conf"),
+                "--model_name", "HadGEM3",
+                "--output_directory", tmpdir
+            ]
+            main()
+
+            expected_file = os.path.join(tmpdir, "mappings_view_HadGEM3.html")
+            self.assertTrue(os.path.exists(expected_file), f"Expected HTML file not found: {expected_file}")
+
+
+if __name__ == '__main__':
+    unittest.main()
