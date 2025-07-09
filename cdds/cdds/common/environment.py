418a06fc14ac9f08f6f43c39715ac909a2b4beb3
Matthew Mizielinski <matthew.mizielinski@metoffice.gov.uk>
2025-07-09 11:04:18 +0000

    Issue #571: Remove unused CYLC path modification code (#572)
    * Issue #571: Remove unused CYLC path modification code

--- cdds/cdds/common/environment.py
+++ cdds/cdds/common/environment.py
@@ -1,31 +1,7 @@
 # (C) British Crown Copyright 2022-2025, Met Office.
 # Please see LICENSE.md for license details.
 
-import re
 import sys
-
-from cdds.common.constants import CYLC_PATHS
-
-
-def fix_import_paths():
-    """
-    Reorders python import paths, moving cylc and rose directories at the end
-    of the list of imports. This is done to prevent code from using versions
-    of some libraries (such as jinja2) included in rose environment, instead of
-    ones installed with the CDDS environment, when running convert code as a
-    rose suite.
-
-    Returns
-    -------
-
-    """
-
-    for path_reg in CYLC_PATHS:
-        r = re.compile(path_reg)
-        path = list(filter(r.match, sys.path))
-        if path:
-            sys.path.remove(path[0])
-            sys.path.append(path[0])
 
 
 def add_to_path(path: str) -> None:
