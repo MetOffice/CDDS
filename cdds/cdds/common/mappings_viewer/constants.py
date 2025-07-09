e5c46d7d858dd3883e363c7b1f76a95e7987b6f1
mo-gill <edward.gillett@metoffice.gov.uk>
2025-07-09 08:22:08 +0000

    cherry pick #474: Update generate_mappings_html. (#573)
    * #474: Fix generate_mappings_html.
    
    * #474: Add filters, adjust templating, removed links.
    
    * #474: Add command line args, not fully functional yet.
    
    * #474:Add throwaway plugin. Raises bootstrap errors.
    
    * #474: Now working for using external plugin.
    
    * #474: Reimplement solution, add warning.
    
    * #474: Add a functional test.
    
    * #474: Remove html folder used for iterating.
    
    * #474: Correct an import.
    
    * #474: Amend copyright.

--- cdds/cdds/common/mappings_viewer/constants.py
+++ cdds/cdds/common/mappings_viewer/constants.py
@@ -1,9 +1,10 @@
 # (C) British Crown Copyright 2022-2025, Met Office.
 # Please see LICENSE.md for license details.
 HEADINGS = ['Variable', 'Mip Table', 'Expression', 'Comments', 'Dimensions', 'Units', 'Component', 'Status', 'File']
-HEADER_ROW_TEMPLATE = '  <thead><tr bgcolor="{}">\n{}  </tr></thead>\n'
+HEADER_ROW_TEMPLATE = ('  <thead>\n   <tr bgcolor="{0}">\n{1}   </tr>\n   \
+                       <tr class="filters">\n{2}   </tr>\n   </thead>\n')
 ROW_TEMPLATE = '  <tr bgcolor="{}">\n{}  </tr>\n'
-CELL_TEMPLATE = '   <{0}>{1}</{0}>\n'
+CELL_TEMPLATE = '     <{0}>{1}</{0}>\n'
 TABLE_TEMPLATE = '<table border=1, id="table_id", class="display">\n{}</table>\n'
 CODE_CELL_TEMPLATE = '   <td><pre><code>{}</code></pre></td>\n'
 TOOLTIP_TEMPLATE = '<div class="tooltip">{}<span class="tooltiptext">{}</span></div>'
@@ -15,19 +16,74 @@ BGCOLORS = ['#E0EEFF', '#FFFFFF']
 HEADER = """
 <html>
 <head>
-<link rel="stylesheet" type="text/css" charset="UTF-8" href="../src/jquery.dataTables-1.11.4.min.css" />
-<script type="text/javascript" charset="UTF-8" src="../src/jquery-3.6.0.slim.min.js"></script>
-<script type="text/javascript" charset="UTF-8" src="../src/jquery.dataTables-1.11.4.min.js"></script>
+<link rel="stylesheet" type="text/css" charset="UTF-8"
+href="https://cdn.datatables.net/2.3.2/css/dataTables.dataTables.min.css"/>
+<script type="text/javascript" charset="UTF-8" src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+<script type="text/javascript" charset="UTF-8" src="https://cdn.datatables.net/2.3.2/js/dataTables.min.js"></script>
 <script type="text/javascript">
-//<![CDATA[
-$(document).ready( function () {
-    $('#table_id').DataTable({"pageLength": 100});
-    } );
-//]]>
+$(document).ready(function () {
+  var table = $('#table_id').DataTable({
+    orderCellsTop: true,
+    fixedHeader: true,
+    pageLength: 100,
+    initComplete: function () {
+      var api = this.api();
+
+      // Show the tables once DataTables has initialized
+      $('#table_id').css('visibility', 'visible');
+
+      // For each column, add a select filter to the second row
+      api.columns().eq(0).each(function (colIdx) {
+        var cell = $('.filters th').eq(colIdx);
+        if (cell.length) {
+          var select = $('<select><option value="">All</option></select>')
+            .appendTo(cell.empty())
+            .on('change', function () {
+              api.column(colIdx)
+                .search(this.value ? '^' + this.value + '$' : '', true, false)
+                .draw();
+            });
+
+          api.column(colIdx).data().unique().sort().each(function (d) {
+            if (d) select.append('<option value="' + d + '">' + d + '</option>');
+          });
+        }
+      });
+    }
+  });
+});
 </script>
 </head>
 <style>
 
+  /* Hide table until DataTables has fully initialized */
+    #table_id {
+    visibility: hidden;
+   }
+
+  /* Format the datatables */
+  table.dataTable {
+    table-layout: fixed;
+    width: 100%;
+  }
+
+  table.dataTable td {
+    max-width: 300px;
+    white-space: normal;
+    word-wrap: break-word;
+  }
+
+  pre, code {
+    white-space: pre-wrap;
+    word-break: break-word;
+  }
+
+  table.dataTable thead select {
+    width: 100%;
+    box-sizing: border-box;
+    padding: 4px;
+    font-size: 0.9em;
+  }
 
    /* Tooltip container */
    .tooltip {
@@ -41,11 +97,11 @@ $(document).ready( function () {
      visibility: hidden;
      bottom: 100%;
      left: 50%;
-     width: 600;
+     width: 600px;
      background-color: #FFFFFF;
      color: black;
      text-align: left;
-     padding: 18px 18px 18px 18px;
+     padding: 18px;
      border-radius: 4px;
      border: 1px solid #000;
 
