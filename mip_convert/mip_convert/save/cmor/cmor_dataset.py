18eb38e3aaef98403d380a8f3220a8f9b2cd33bd
Matthew Mizielinski <matthew.mizielinski@metoffice.gov.uk>
2025-07-11 08:23:22 +0000

    Exploration:
    * New mappings plugin for HadGEM3GC5 to separate mappings for branded
      variables
    * Changes to add a call to cmor wrapper to clearly set the frequency
    * Changes needed to interpret new MIP tables correctly
    * Example mip_convert config file
    
    The CMIP7_CV.json file being used is slightly modified relative to the
    one in github as it has additional entries for "mip_era" and
    "parent_mip_era" which have been missed.

--- mip_convert/mip_convert/save/cmor/cmor_dataset.py
+++ mip_convert/mip_convert/save/cmor/cmor_dataset.py
@@ -152,7 +152,10 @@ class Dataset(object):
         self._items.update({'history': self._user_config.history})
         # Add whether CMIP6 validation should be performed.
         if not self._relaxed_cmor:
-            self._items.update({'_cmip6_option': 'CMIP6'})
+            if self._items['mip_era'].startswith('CMIP7'):
+                self._items.update({'_cmip7_option': 'CMIP7'})
+            else:
+                self._items.update({'_cmip6_option': 'CMIP6'})
         # Add the items that can be determined from the 'variant_label'.
         self._items.update(self._items_from_variant_label)
         # Add the items that can be determined from the CV file.
