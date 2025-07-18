1a462034e6a24bdabeb39bac2aaeb4f3c467f2c7
Jared Drayton <68954395+mo-jareddrayton@users.noreply.github.com>
2025-07-18 11:01:48 +0100

    #592 refactor duplicate CVConfig classes (#599)
    * #592 Refactor CVConfig into one Class
    
    * #592 Remove old CVConfig
    
    * #592 Add comment for CMIP6 case.
    
    Co-authored-by: Matthew Mizielinski <matthew.mizielinski@metoffice.gov.uk>
    
    ---------
    
    Co-authored-by: Matthew Mizielinski <matthew.mizielinski@metoffice.gov.uk>

--- mip_convert/mip_convert/configuration/cv_config.py
+++ mip_convert/mip_convert/configuration/cv_config.py
@@ -162,11 +162,17 @@ class CVConfig(JSONConfig):
             return self._UNKNOWN
         # Otherwise attempt to get parent activity id from parentage
         # dictionary
-        parentage_dict = self._get_parentage_dictionary(info)
-        if parent_experiment_id not in parentage_dict:
-            error_message = 'Parent experiment id "{}" not found in parentage dictionary: "{}"'
-            raise RuntimeError(error_message.format(parent_experiment_id, parentage_dict))
-        return parentage_dict[parent_experiment_id]
+        # CMIP6 had multiple options for the parent activity id of an experiment, which needed to be
+        # handled separately. Not sure if this is true for CMIP7 yet.
+        if mip_era == 'CMIP6':
+            parentage_dict = self._get_parentage_dictionary(info)
+            if parent_experiment_id not in parentage_dict:
+                error_message = 'Parent experiment id "{}" not found in parentage dictionary: "{}"'
+                raise RuntimeError(error_message.format(parent_experiment_id, parentage_dict))
+            return parentage_dict[parent_experiment_id]
+        else:
+            # Retained for compatibility with CMIP5 and other projects
+            return self._get_single_value(CVKey.PARENT_ACTIVITY_ID, info)
 
     def source(self, source_id):
         """
