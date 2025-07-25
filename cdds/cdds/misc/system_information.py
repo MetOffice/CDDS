d24ee6db3e8c3a933450ab8f50197ff173ccafbe
mo-gill <edward.gillett@metoffice.gov.uk>
2025-07-25 08:17:14 +0000

    #580: Refine code. Add wrapper. Run pep8.

--- cdds/cdds/misc/system_information.py
+++ cdds/cdds/misc/system_information.py
@@ -1,10 +1,8 @@
+#!/usr/bin/env python3
 # (C) British Crown Copyright 2025, Met Office.
 # Please see LICENSE.md for license details.
-# function that iterates over env vars in turn and prints them. If any arent set  or paths dont exist it should be flagged provides 1 exit code if it fails
-#put wrapper into bin
 import os
-import argparse
-# from cdds import _DEV
+
 
 def check_environment_variables():
     """
@@ -13,7 +11,7 @@ def check_environment_variables():
     """
     required_vars = [
         'CYLC_VERSION',
-        'TZ'
+        'TZ',
         'CDDS_PLATFORM',
         'CDDS_ETC',
         'CDDS_ENV_COMMAND',
@@ -28,29 +26,36 @@ def check_environment_variables():
     unset_vars = []
     unresolved_paths = []
 
-    # Print required environment variables from environ and notify user if any are not set.
     for var in required_vars:
         x = env_vars.get(var)
         print(f"{var}: {x}")
-        if x is None:
+        if not x:
             unset_vars.append(var)
-            print(f"Required environment variable '{var}' is not set.")
 
     # Check variables with required paths are valid.
     for var in required_path_vars:
-        expanded_value = os.path.expandvars(env_vars[var])
+        value = env_vars.get(var)
+        # If path var is unset, continue to avoid path expansion error.
+        if not value:
+            continue
+        # Expand and validate path environment variables.
+        expanded_value = os.path.expandvars(value)
         if expanded_value.startswith('source '):
             expanded_value = expanded_value[len('source '):].strip()
         if not os.path.exists(expanded_value):
             unresolved_paths.append(var)
-            print(f"Path '{expanded_value}' for '{var}' is not valid.")
+
+    if unset_vars or unresolved_paths:
+        print("\nIssues detected with environment variables:")
+        for x in unset_vars:
+            print(f"Required environment variable '{x}' is not set.")
+        for x in unresolved_paths:
+            print(f"Filepath for environment variable '{x}' is invalid.")
 
     if unset_vars or unresolved_paths:
         exit_code = 1
-    
+
     else:
         exit_code = 0
 
-check_environment_variables()
-
-
+    return exit_code
