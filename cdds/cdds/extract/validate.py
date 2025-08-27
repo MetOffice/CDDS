1d2ae857762e4d90dcf3c90739332af2b99c346b
Jared Drayton <68954395+mo-jareddrayton@users.noreply.github.com>
2025-08-27 10:44:32 +0000

    Issue #494: cherry-pick fix failing stream validation for daily Gregorian data (#618)

--- cdds/cdds/extract/validate.py
+++ cdds/cdds/extract/validate.py
@@ -4,17 +4,25 @@
 import logging
 import os
 
-from cdds.common import generate_datestamps_pp, generate_datestamps_nc
-from cdds.common.plugins.plugins import PluginStore
+from metomi.isodatetime.data import Calendar
+
+from cdds.common import generate_datestamps_nc, generate_datestamps_pp
 from cdds.common.cdds_files.cdds_directories import component_directory, log_directory
+from cdds.common.plugins.plugins import PluginStore
 from cdds.common.request.request import read_request
 from cdds.extract.common import (
-    configure_mappings, configure_variables,
-    get_data_target, get_streamtype,
-    validate_stash_fields, validate_netcdf,
-    StreamValidationResult, build_mass_location,
+    FileContentError,
+    StashError,
+    StreamValidationResult,
+    build_mass_location,
+    configure_mappings,
+    configure_variables,
+    get_data_target,
+    get_stash_from_pp,
+    get_streamtype,
+    validate_netcdf,
 )
-from cdds.extract.constants import STREAMTYPE_PP, STREAMTYPE_NC
+from cdds.extract.constants import STREAMTYPE_NC, STREAMTYPE_PP
 from cdds.extract.filters import Filters
 
 
@@ -68,7 +76,7 @@ def validate_streams(streams, args):
             for sub_stream in substreams:
                 filenames += mappings.generate_filenames_nc(datestamps, sub_stream)
 
-        validate(data_target, stream, stash_codes, stream_validation, filenames)
+        validate(data_target, stream, stash_codes, stream_validation, filenames, file_frequency)
     else:
         logger.info('skipped [{}]: there are no variables requiring this stream'.format(stream))
     stream_validation.log_results(log_directory(request, "extract"))
@@ -76,7 +84,7 @@ def validate_streams(streams, args):
     return stream_validation
 
 
-def validate(path, stream, stash_codes, validation_result, filenames):
+def validate(path, stream, stash_codes, validation_result, filenames, file_frequency):
     """Simple validation based on checking correct number of files have
     been extracted, and stash codes comparison in the case of pp streams.
 
@@ -96,7 +104,11 @@ def validate(path, stream, stash_codes, validation_result, filenames):
     streamtype = get_streamtype(stream)
     validate_file_names(path, validation_result, filenames, streamtype)
     if streamtype == STREAMTYPE_PP:
-        validate_directory_pp(path, stash_codes, validation_result)
+        logger = logging.getLogger(__name__)
+        logger.info("Checking STASH fields")
+        stash_in_file = get_stash_fields(path, validation_result)
+        check_expected_stash(stash_in_file, validation_result, path, stash_codes)
+        check_consistent_stash(stash_in_file, validation_result, path, file_frequency)
     elif streamtype == STREAMTYPE_NC:
         validate_directory_netcdf(path, validation_result)
 
@@ -125,29 +137,6 @@ def validate_file_names(path, validation_result, filenames, file_type):
     validation_result.add_file_names(expected_files, actual_files)
 
 
-def validate_directory_pp(path, stash_codes, validation_result):
-    """
-    Checks that PP files at provided location can be read and
-    contain |STASH| codes consistent with the reference
-    set given as a second argument.
-    Returns overall validation status, error message(s),
-    and a listof unreadable files (if present).
-
-    Parameters
-    ----------
-    path: str
-        Path pointing to the location of a set of PP files.
-    stash_codes: set
-        A set of unique |STASH| codes that the set of files will
-        be validated against.
-    validation_result: cdds.common.StreamValidationResult
-        An object to hold results from the stream validation
-    """
-    logger = logging.getLogger(__name__)
-    logger.info("Checking STASH fields")
-    validate_stash_fields(path, stash_codes, validation_result)
-
-
 def validate_directory_netcdf(path, validation_result):
     """
     Checks that |netCDF| files at provided location can be read.
@@ -168,3 +157,93 @@ def validate_directory_netcdf(path, validation_result):
             error = validate_netcdf(os.path.join(root, datafile))
             if error is not None:
                 validation_result.add_file_content_error(error)
+
+
+def get_stash_fields(path: str, validation_result: StreamValidationResult) -> dict[str, dict[str, int]]:
+    """ Validates if pp files in a given location contain all required
+    stash codes,
+
+    :param path: Path of interest.
+    :type path: str
+    :param validation_result: Holds errors and results of validation.
+    :type validation_result: StreamValidationResult
+    :return:
+    :rtype: dict[in, int]
+    """
+    stash_in_file = {}
+    pp_files = [file for file in os.listdir(path) if file.endswith('.pp')]
+
+    if pp_files:
+        for pp_file in sorted(pp_files):
+            stash = get_stash_from_pp(os.path.join(path, pp_file))
+            if stash is None:
+                validation_result.add_file_content_error(
+                    FileContentError(os.path.join(path, pp_file), "unreadable file"))
+            stash_in_file[pp_file] = stash
+    return stash_in_file
+
+
+def check_expected_stash(
+    stash_in_file: dict[str, dict[int, int]],
+    validation_result: StreamValidationResult,
+    path: str,
+    expected_stash: set[int],
+):
+    """ Checks that all the expected stash codes are found in each file.
+
+    :param stash_in_file: Stash entries in files.
+    :type stash_in_file: dict[str, dict[int, int]]
+    :param validation_result: Validation results for a given stream.
+    :type validation_result: StreamValidationResult
+    :param path: Path to files.
+    :type path: str
+    :param expected_stash: The set of expected stash codes.
+    :type expected_stash: set[int]
+    """
+    for file, stash in stash_in_file.items():
+        if expected_stash.difference(set(stash.keys())):
+            stash_diff = expected_stash.difference(set(stash.keys()))
+            if stash_diff:
+                error = StashError(os.path.join(path, file), "STASH errors")
+                for diff in stash_diff:
+                    error.add_stash_error(diff)
+                validation_result.add_file_content_error(error)
+
+
+def check_consistent_stash(
+    stash_in_file: dict[str, dict[str, int]],
+    validation_result: StreamValidationResult,
+    path: str,
+    file_frequency: str
+) -> None:
+    """ Checks that the number of stash entries is consistent across all files.
+
+    :param stash_in_file: Stash entries in files.
+    :type stash_in_file: dict
+    :param validation_result: Validation results for a given stream.
+    :type validation_result: StreamValidationResult
+    :param path: Path to files.
+    :type path: str
+    """
+    if Calendar.default().mode == "gregorian" and file_frequency in ["monthly", "seasonal"]:
+        logger = logging.getLogger(__name__)
+        logger.info(
+            f"Skipping stash consistency check as CDDS does not currently validate \
+            {file_frequency} files when using the gregorian calendar."
+        )
+        return None
+
+    reference_file: str
+    reference_stash: dict[str, int]
+    reference_file, reference_stash = next(iter(stash_in_file.items()))
+
+    for file, stash in stash_in_file.items():
+        if reference_stash != stash:
+            error = StashError(os.path.join(path, file), f"STASH errors relative to reference file {reference_file}")
+            for key, value in reference_stash.items():
+                if key not in stash or stash[key] != value:
+                    error.add_stash_error(key)
+            for key in stash:
+                if key not in reference_stash:
+                    error.add_stash_error(key)
+            validation_result.add_file_content_error(error)
