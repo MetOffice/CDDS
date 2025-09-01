ffc508964655281684c26bd1339abca75dcc36f7
Jared Drayton <68954395+mo-jareddrayton@users.noreply.github.com>
2025-09-01 10:21:50 +0100

    Issue #624 Restore development mode.

--- cdds/cdds/__init__.py
+++ cdds/cdds/__init__.py
@@ -2,6 +2,6 @@
 # Please see LICENSE.md for license details.
 from cdds.versions import get_version
 
-_DEV = False
-_NUMERICAL_VERSION = '3.2.1'
+_DEV = True
+_NUMERICAL_VERSION = '3.2.2'
 __version__ = get_version('cdds')
