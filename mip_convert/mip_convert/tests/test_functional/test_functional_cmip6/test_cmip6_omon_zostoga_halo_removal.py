9d4cfcc244d71517f9056a35011458e131c1a056
mo-gill <edward.gillett@metoffice.gov.uk>
2025-09-10 07:42:36 +0000

    #547: Remove a blank line.

--- mip_convert/mip_convert/tests/test_functional/test_functional_cmip6/test_cmip6_omon_zostoga_halo_removal.py
+++ mip_convert/mip_convert/tests/test_functional/test_functional_cmip6/test_cmip6_omon_zostoga_halo_removal.py
@@ -1,7 +1,6 @@
 # (C) British Crown Copyright 2022-2025, Met Office.
 # Please see LICENSE.md for license details.
 import os
-
 import pytest
 
 from mip_convert.tests.test_functional.test_command_line import AbstractFunctionalTests
