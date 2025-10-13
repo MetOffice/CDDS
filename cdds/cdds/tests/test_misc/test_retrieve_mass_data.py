e6ee61c7aa2ece849d887047fa27c9f335c6ce0e
mo-gill <edward.gillett@metoffice.gov.uk>
2025-10-13 14:35:45 +0000

    #628: Use mkdtemp, add teardown.

--- cdds/cdds/tests/test_misc/test_retrieve_mass_data.py
+++ cdds/cdds/tests/test_misc/test_retrieve_mass_data.py
@@ -1,7 +1,9 @@
 # (C) British Crown Copyright 2025, Met Office.
 # Please see LICENSE.md for license details.
+import shutil
 import unittest
 from pathlib import Path
+from tempfile import mkdtemp
 from unittest.mock import patch
 
 from cdds.misc.retrieve_mass_data import transfer_files
@@ -9,11 +11,15 @@ from cdds.misc.retrieve_mass_data import transfer_files
 
 class TestTransferFiles(unittest.TestCase):
     MASS_COMMAND_OUTPUT = "#### get, command-id=2059286267, estimated-cost=60722658byte(s), files=1, media=0"
+
     def setUp(self):
         self.dummy_chunks = [["file1.nc", "file2.nc"], ["file3.nc"]]
-        self.tmp_dir = Path("/tmp/test_output_dir")
+        self.tmp_dir = Path(mkdtemp(prefix="test_output_dir_"))
         self.output_dir = self.tmp_dir / "output"
         self.output_dir.mkdir(parents=True, exist_ok=True)
+
+    def tearDown(self):
+        shutil.rmtree(self.tmp_dir)
 
     @patch("cdds.misc.retrieve_mass_data.run_mass_command")
     @patch("logging.getLogger")
