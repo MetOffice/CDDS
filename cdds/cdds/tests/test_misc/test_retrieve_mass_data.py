fb02458d7b82422430ff605c28eba6e50105fafc
mo-gill <edward.gillett@metoffice.gov.uk>
2025-10-13 08:23:25 +0000

    #628: Some reformatting and linting.

--- cdds/cdds/tests/test_misc/test_retrieve_mass_data.py
+++ cdds/cdds/tests/test_misc/test_retrieve_mass_data.py
@@ -1,8 +1,11 @@
-
+# (C) British Crown Copyright 2025, Met Office.
+# Please see LICENSE.md for license details.
 import unittest
 from pathlib import Path
 from unittest.mock import patch
+
 from cdds.misc.retrieve_mass_data import transfer_files
+
 
 class TestTransferFiles(unittest.TestCase):
     def setUp(self):
@@ -14,20 +17,31 @@ class TestTransferFiles(unittest.TestCase):
     @patch("cdds.misc.retrieve_mass_data.run_mass_command")
     @patch("cdds.misc.retrieve_mass_data.logger")
     def test_transfer_files_dry_run(self, mock_logger, mock_run_mass_command):
-        mock_run_mass_command.return_value = ("#### get, command-id=2059286267, "
-        "estimated-cost=60722658byte(s), files=1, media=0",)
+        mock_run_mass_command.return_value = (
+            "#### get, command-id=2059286267, "
+            "estimated-cost=60722658byte(s), files=1, media=0",
+        )
         transfer_files(self.dummy_chunks, self.output_dir, dry_run=True)
         self.assertEqual(mock_run_mass_command.call_count, len(self.dummy_chunks))
         for call in mock_logger.info.call_args_list:
-            self.assertIn(("#### get, command-id=2059286267, "
-            "estimated-cost=60722658byte(s), files=1, media=0"), call[0][0])
+            self.assertIn(
+                (
+                    "#### get, command-id=2059286267, "
+                    "estimated-cost=60722658byte(s), files=1, media=0"
+                ),
+                call[0][0],
+            )
 
-    @patch("cdds.misc.retrieve_mass_data.run_mass_command", side_effect=RuntimeError("fail"))
+    @patch(
+        "cdds.misc.retrieve_mass_data.run_mass_command",
+        side_effect=RuntimeError("fail"),
+    )
     @patch("cdds.misc.retrieve_mass_data.logger")
     def test_transfer_files_runtime_error(self, mock_logger, mock_run_mass_command):
         with self.assertRaises(RuntimeError):
             transfer_files(self.dummy_chunks, self.output_dir, dry_run=True)
         self.assertTrue(mock_logger.critical.called)
+
 
 if __name__ == "__main__":
     unittest.main()
