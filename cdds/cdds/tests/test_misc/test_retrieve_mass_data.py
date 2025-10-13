043089c89c0bf07c899c7580c3a0cd629861d7cb
mo-gill <edward.gillett@metoffice.gov.uk>
2025-10-13 13:19:53 +0000

    #628: Reduce duplication.

--- cdds/cdds/tests/test_misc/test_retrieve_mass_data.py
+++ cdds/cdds/tests/test_misc/test_retrieve_mass_data.py
@@ -8,6 +8,7 @@ from cdds.misc.retrieve_mass_data import transfer_files
 
 
 class TestTransferFiles(unittest.TestCase):
+    MASS_COMMAND_OUTPUT = "#### get, command-id=2059286267, estimated-cost=60722658byte(s), files=1, media=0"
     def setUp(self):
         self.dummy_chunks = [["file1.nc", "file2.nc"], ["file3.nc"]]
         self.tmp_dir = Path("/tmp/test_output_dir")
@@ -15,17 +16,15 @@ class TestTransferFiles(unittest.TestCase):
         self.output_dir.mkdir(parents=True, exist_ok=True)
 
     @patch("cdds.misc.retrieve_mass_data.run_mass_command")
-    @patch("logging.info")
-    def test_transfer_files_dry_run(self, mock_info, mock_run_mass_command):
-        mock_run_mass_command.return_value = (
-            "#### get, command-id=2059286267, "
-            "estimated-cost=60722658byte(s), files=1, media=0",
-        )
+    @patch("logging.getLogger")
+    def test_transfer_files_dry_run(self, mock_get_logger, mock_run_mass_command):
+        mock_logger = mock_get_logger.return_value
+        mock_run_mass_command.return_value = (self.MASS_COMMAND_OUTPUT,)
         transfer_files(self.dummy_chunks, self.output_dir, dry_run=True)
         self.assertEqual(mock_run_mass_command.call_count, len(self.dummy_chunks))
-        for call in mock_info.call_args_list:
+        for call in mock_logger.info.call_args_list:
             self.assertIn(
-                "#### get, command-id=2059286267, estimated-cost=60722658byte(s), files=1, media=0",
+                self.MASS_COMMAND_OUTPUT,
                 call[0][0],
             )
 
