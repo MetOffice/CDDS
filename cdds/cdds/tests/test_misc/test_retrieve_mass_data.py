7a3c89c9aa2010e0c5a12cd044efeaa051bd719f
mo-gill <edward.gillett@metoffice.gov.uk>
2025-10-15 13:46:31 +0000

    #628: fix tests.

--- cdds/cdds/tests/test_misc/test_retrieve_mass_data.py
+++ cdds/cdds/tests/test_misc/test_retrieve_mass_data.py
@@ -14,19 +14,20 @@ class TestTransferFiles(unittest.TestCase):
 
     def setUp(self):
         self.dummy_chunks = [["file1.nc", "file2.nc"], ["file3.nc"]]
-        self.tmp_dir = Path(mkdtemp(prefix="test_output_dir_"))
-        self.output_dir = self.tmp_dir / "output"
+        self.TMPDIR = Path(mkdtemp(prefix="test_tmp_dir_"))
+        self.tmp_output_dir = Path(mkdtemp(prefix="test_output_dir_"))
+        self.output_dir = self.tmp_output_dir / "output"
         self.output_dir.mkdir(parents=True, exist_ok=True)
 
     def tearDown(self):
-        shutil.rmtree(self.tmp_dir)
+        shutil.rmtree(self.tmp_output_dir)
 
     @patch("cdds.misc.retrieve_mass_data.run_mass_command")
     @patch("logging.getLogger")
     def test_transfer_files_dry_run(self, mock_get_logger, mock_run_mass_command):
         mock_logger = mock_get_logger.return_value
         mock_run_mass_command.return_value = (self.MASS_COMMAND_OUTPUT,)
-        transfer_files(self.dummy_chunks, self.output_dir, dry_run=True)
+        transfer_files(self.dummy_chunks, self.TMPDIR, self.output_dir, dry_run=True)
         self.assertEqual(mock_run_mass_command.call_count, len(self.dummy_chunks))
         for call in mock_logger.info.call_args_list:
             self.assertIn(
@@ -42,7 +43,7 @@ class TestTransferFiles(unittest.TestCase):
     def test_transfer_files_runtime_error(self, mock_get_logger, mock_run_mass_command):
         mock_logger = mock_get_logger.return_value
         with self.assertRaises(RuntimeError):
-            transfer_files(self.dummy_chunks, self.output_dir, dry_run=True)
+            transfer_files(self.dummy_chunks, self.TMPDIR, self.output_dir, dry_run=True)
         self.assertTrue(mock_logger.critical.called)
 
 
