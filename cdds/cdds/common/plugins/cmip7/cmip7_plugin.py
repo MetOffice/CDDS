fce274bf07ad35040a7afaf096871f8c7cea76dd
Jared Drayton <68954395+mo-jareddrayton@users.noreply.github.com>
2025-11-04 14:50:16 +0000

    Issue #668: Add correct CMIP7 license

--- cdds/cdds/common/plugins/cmip7/cmip7_plugin.py
+++ cdds/cdds/common/plugins/cmip7/cmip7_plugin.py
@@ -20,24 +20,7 @@ if TYPE_CHECKING:
     from cdds.common.request.request import Request
 
 
-CMIP7_LICENSE = ('CMIP7 model data produced by MOHC is licensed '
-                 'under a Creative Commons Attribution ShareAlike '
-                 '4.0 International License '
-                 '(https://creativecommons.org/licenses). Consult '
-                 'https://pcmdi.llnl.gov/CMIP7/TermsOfUse for '
-                 'terms of use governing CMIP7 output, including '
-                 'citation requirements and proper acknowledgment. '
-                 'Further information about this data, including '
-                 'some limitations, can be found via the '
-                 'further_info_url (recorded as a global attribute '
-                 'in this file) . The data producers and data '
-                 'providers make no warranty, either express or '
-                 'implied, including, but not limited to, '
-                 'warranties of merchantability and fitness for a '
-                 'particular purpose. All liabilities arising from '
-                 'the supply of the information (including any '
-                 'liability arising in negligence) are excluded to '
-                 'the fullest extent permitted by law.')
+CMIP7_LICENSE = "CC-BY 4.0"
 
 
 class Cmip7Plugin(BasePlugin):
