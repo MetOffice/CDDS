fe5d9bf50c9b7bb19604e0a4da77e1445428dd7e
Jared Drayton <68954395+mo-jareddrayton@users.noreply.github.com>
2025-11-04 13:26:46 +0000

    Issue #668: Remove CMIP6 references from CMIP7 project plugin

--- cdds/cdds/common/plugins/cmip7/cmip7_plugin.py
+++ cdds/cdds/common/plugins/cmip7/cmip7_plugin.py
@@ -1,7 +1,7 @@
 # (C) British Crown Copyright 2021-2025, Met Office.
 # Please see LICENSE.md for license details.
 """
-The :mod:`cmip6_plugin` module contains the code for the CMIP6 plugin.
+The :mod:`cmip7_plugin` module contains the code for the CMIP7 plugin.
 """
 import os
 from typing import Type, Dict, Any, TYPE_CHECKING
@@ -12,20 +12,20 @@ from cdds.common.plugins.models import ModelParameters
 from cdds.common.plugins.streams import StreamInfo
 from cdds.common.plugins.base.base_plugin import BasePlugin, MipEra
 from cdds.common.plugins.base.base_models import BaseModelParameters
-from cdds.common.plugins.cmip7.cmip6_attributes import Cmip6GlobalAttributes
-from cdds.common.plugins.cmip7.cmip6_grid import Cmip6GridLabel
-from cdds.common.plugins.cmip7.cmip6_models import Cmip6ModelsStore
-from cdds.common.plugins.cmip7.cmip6_streams import Cmip6StreamStore
+from cdds.common.plugins.cmip7.cmip7_attributes import Cmip7GlobalAttributes
+from cdds.common.plugins.cmip7.cmip7_grid import Cmip7GridLabel
+from cdds.common.plugins.cmip7.cmip7_models import Cmip7ModelsStore
+from cdds.common.plugins.cmip7.cmip7_streams import Cmip7StreamStore
 if TYPE_CHECKING:
     from cdds.common.request.request import Request
 
 
-CMIP6_LICENSE = ('CMIP6 model data produced by MOHC is licensed '
+CMIP7_LICENSE = ('CMIP7 model data produced by MOHC is licensed '
                  'under a Creative Commons Attribution ShareAlike '
                  '4.0 International License '
                  '(https://creativecommons.org/licenses). Consult '
-                 'https://pcmdi.llnl.gov/CMIP6/TermsOfUse for '
-                 'terms of use governing CMIP6 output, including '
+                 'https://pcmdi.llnl.gov/CMIP7/TermsOfUse for '
+                 'terms of use governing CMIP7 output, including '
                  'citation requirements and proper acknowledgment. '
                  'Further information about this data, including '
                  'some limitations, can be found via the '
@@ -40,72 +40,72 @@ CMIP6_LICENSE = ('CMIP6 model data produced by MOHC is licensed '
                  'the fullest extent permitted by law.')
 
 
-class Cmip6Plugin(BasePlugin):
+class Cmip7Plugin(BasePlugin):
     """
-    Plugin for CMIP6 models
+    Plugin for CMIP7 models
     """
 
     def __init__(self):
-        super(Cmip6Plugin, self).__init__(MipEra.CMIP7.value)
+        super(Cmip7Plugin, self).__init__(MipEra.CMIP7.value)
 
     def models_parameters(self, model_id: str) -> ModelParameters:
         """
-        Returns the model parameters of the CMIP6 model with given model id.
+        Returns the model parameters of the CMIP7 model with given model id.
 
         :param model_id: Model ID
         :type model_id: str
         :return: Model parameters of model
         :rtype: BaseModelParameters
         """
-        models_store = Cmip6ModelsStore.instance()
+        models_store = Cmip7ModelsStore.instance()
         return models_store.get(model_id)
 
     def overload_models_parameters(self, source_dir: str) -> None:
         """
-        Overloads model parameters of CMIP6 models. The new parameters are
+        Overloads model parameters of CMIP7 models. The new parameters are
         specified in a json file in the given directory. The json file name
         must match following pattern: <model-name>.json
 
         :param source_dir: Path to the directory containing the files specifies the new values
         :type source_dir: str
         """
-        models_store = Cmip6ModelsStore.instance()
+        models_store = Cmip7ModelsStore.instance()
         models_store.overload_params(source_dir)
 
     def grid_labels(self) -> Type[GridLabel]:
         """
-        Returns the grid labels related to CMIP6 models.
+        Returns the grid labels related to CMIP7 models.
 
         :return: Grid labels
-        :rtype: Cmip6GridLabel
+        :rtype: Cmip7GridLabel
         """
-        return Cmip6GridLabel
+        return Cmip7GridLabel
 
     def stream_info(self) -> StreamInfo:
         """
-        Returns the information of streams related to CMIP6.
+        Returns the information of streams related to CMIP7.
 
         :return: Information of streams
         :rtype: StreamInfo
         """
-        stream_store = Cmip6StreamStore.instance()
+        stream_store = Cmip7StreamStore.instance()
         return stream_store.get()
 
-    def global_attributes(self, request: "Request") -> Cmip6GlobalAttributes:
+    def global_attributes(self, request: "Request") -> Cmip7GlobalAttributes:
         """
-        Returns the global attributes for CMIP6. The given request contains all information
+        Returns the global attributes for CMIP7. The given request contains all information
         about the global attributes.
 
         :param request: Dictionary containing information about the global attributes
         :type request: Dict[str, Any]
-        :return: Class to store and manage the global attributes for CMIP6
-        :rtype: Cmip6GlobalAttributes
+        :return: Class to store and manage the global attributes for CMIP7
+        :rtype: Cmip7GlobalAttributes
         """
-        return Cmip6GlobalAttributes(request)
+        return Cmip7GlobalAttributes(request)
 
     def model_file_info(self) -> ModelFileInfo:
         """
-        Returns information to the simulation model files related to CMIP6.
+        Returns information to the simulation model files related to CMIP7.
 
         :return: Information to the simulation model files
         :rtype: GlobalModelFileInfo
@@ -114,18 +114,18 @@ class Cmip6Plugin(BasePlugin):
 
     def license(self) -> str:
         """
-        Returns the license for CMIP6
+        Returns the license for CMIP7
 
         :return: License
         :rtype: str
         """
-        return CMIP6_LICENSE
+        return CMIP7_LICENSE
 
     def mip_table_dir(self) -> str:
         """
-        Returns the path to the MIP table directory that should be used for CMIP6
+        Returns the path to the MIP table directory that should be used for CMIP7
 
         :return: Path to the MIP table directory
         :rtype: str
         """
-        return os.path.join(os.environ['CDDS_ETC'], 'mip_tables', 'CMIP6', '01.00.29')
+        return os.path.join(os.environ['CDDS_ETC'], 'mip_tables', 'CMIP7', '01.00.29')
--- cdds/cdds/common/plugins/cmip7/cmip7_streams.py
+++ cdds/cdds/common/plugins/cmip7/cmip7_streams.py
@@ -1,15 +1,15 @@
 # (C) British Crown Copyright 2022-2025, Met Office.
 # Please see LICENSE.md for license details.
 """
-The :mod:`cmip6_streams` module contains the code required to
-handle stream information for CMIP6 streams.
+The :mod:`cmip7_streams` module contains the code required to
+handle stream information for CMIP7 streams.
 """
 import os
 
 from cdds.common.plugins.base.base_streams import BaseStreamInfo, BaseStreamStore
 
 
-class Cmip6StreamInfo(BaseStreamInfo):
+class Cmip7StreamInfo(BaseStreamInfo):
     """
     Class to store the information for streams. The information of the streams
     are defined in a json file.
@@ -19,10 +19,10 @@ class Cmip6StreamInfo(BaseStreamInfo):
         if not config_path:
             local_dir = os.path.dirname(os.path.abspath(__file__))
             config_path = os.path.join(local_dir, 'data/streams/streams_config.json')
-        super(Cmip6StreamInfo, self).__init__(config_path)
+        super(Cmip7StreamInfo, self).__init__(config_path)
 
 
-class Cmip6StreamStore(BaseStreamStore):
+class Cmip7StreamStore(BaseStreamStore):
     """
     Singleton class to store for the stream information.
 
@@ -30,15 +30,15 @@ class Cmip6StreamStore(BaseStreamStore):
     """
 
     def __init__(self) -> None:
-        stream_info = Cmip6StreamInfo()
-        super(Cmip6StreamStore, self).__init__(stream_info)
+        stream_info = Cmip7StreamInfo()
+        super(Cmip7StreamStore, self).__init__(stream_info)
 
     @classmethod
-    def create_instance(cls) -> 'Cmip6StreamStore':
+    def create_instance(cls) -> 'Cmip7StreamStore':
         """
         Creates a new class instance.
 
         :return: New class instance
-        :rtype: Cmip6StreamStore
+        :rtype: Cmip7StreamStore
         """
-        return Cmip6StreamStore()
+        return Cmip7StreamStore()
