32fab79f0bae1e08e2807ec770cd7aa5b94cfcb6
mo-gill <edward.gillett@metoffice.gov.uk>
2025-12-01 10:18:55 +0000

    #694: Add path building.

--- cdds/cdds/misc/cdds_repack.py
+++ cdds/cdds/misc/cdds_repack.py
@@ -1,10 +1,12 @@
 import argparse
+import os
 import sys
 from argparse import Namespace
-from pathlib import Path
-from typing import List
 
 from cdds.common.request.request import read_request
+from cdds.common.plugins.plugin_loader import load_plugin
+from cdds.common.cdds_files.cdds_directories import output_data_directory
+
 
 # a tool that takes a request file and stream as an argument.
 
@@ -21,29 +23,79 @@ def parse_repack_args(arguments) -> Namespace:
     """
     user_arguments = arguments
     parser = argparse.ArgumentParser(
-        description='Wrapper for repacking netCDF-4 files to optimize chunking and metadata layout.')
+        description="Wrapper for repacking netCDF-4 files to optimize chunking and metadata layout."
+    )
     parser.add_argument(
-        'request', help=(
-            'The full path to the configuration file containing the information about the request.'
-        ))
+        "request",
+        help=(
+            "The full path to the configuration file containing the information about the request."
+        ),
+    )
     parser.add_argument(
-        'stream', help=(
-            'The name of the stream to repack (e.g., ap4, ap5, apm, onm, inm).'
-        ))
+        "stream",
+        help=("The name of the stream to repack (e.g., ap4, ap5, apm, onm, inm)."),
+    )
 
     args = parser.parse_args(user_arguments)
 
     return args
+
+
 # filepath comes from root_data_dir
 
+
 def main_cdds_repack() -> None:
+    """
+    Main function for cdds_repack that repacks NetCDF4 files for a given stream.
+
+    Takes a request file and stream name as arguments, constructs the output directory
+    path following CDDS structure:
+    {root_data_dir}/{mip_era}/{mip}/{request_id}/{package}/output/{stream}/
+
+    Then finds all mip_table directories within that stream and processes the files.
+    """
     print("repack script running")
     args = parse_repack_args(sys.argv[1:])
+
+    # Load the appropriate plugin based on the request's mip_era
     request = read_request(args.request)
-    stream = args.stream
-    request_data_dir = request.common.root_data_dir
+    load_plugin(request.metadata.mip_era)
+
+    # Get the base output directory of CDDS Convert using CDDS plugin system
+    # This constructs: {root_data_dir}/{mip_era}/{mip}/{model_experiment_variant}/{package}/output
+    base_output_dir = output_data_directory(request)
+    print(f"Base output directory: {base_output_dir}")
+
+    # Add the stream subdirectory
+    stream_output_dir = os.path.join(base_output_dir, args.stream)
+    print(f"Stream output directory: {stream_output_dir}")
+
+    # Check if the stream directory exists
+    if not os.path.exists(stream_output_dir):
+        print(f"ERROR: Stream directory does not exist: {stream_output_dir}")
+        return
+
+    # Find all mip_table directories within this stream
+    # Structure: output/{stream}/{mip_table}/
+    mip_table_dirs = []
+    for item in os.listdir(stream_output_dir):
+        item_path = os.path.join(stream_output_dir, item)
+        if os.path.isdir(item_path):
+            mip_table_dirs.append((item, item_path))
+
+    print(
+        f"Found {len(mip_table_dirs)} mip_table directories in stream '{args.stream}':"
+    )
+    for mip_table, mip_table_path in mip_table_dirs:
+        print(f"  - {mip_table}: {mip_table_path}")
+        # TODO: Find and process NetCDF files in each mip_table directory
+        # nc_files = glob.glob(os.path.join(mip_table_path, "*.nc"))
+        # for nc_file in nc_files:
+        #     run_cmip7repack(nc_file)
+
     breakpoint()
-    
+
+
 # def retrieve_filepaths(request_file):
 #     pass
 
@@ -53,6 +105,4 @@ def main_cdds_repack() -> None:
 #         run_cmip7repack(file_path)
 
 
-
 ####################
-
