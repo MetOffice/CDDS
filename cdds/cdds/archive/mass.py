87deaa7e2cacdf5c2af6d82d232ecb3b3a402d2d
mo-laurenboon <lauren.boon@metoffice.gov.uk>
2025-12-08 10:01:15 +0000

    #716: Improvements added from PR review comments

--- cdds/cdds/archive/mass.py
+++ cdds/cdds/archive/mass.py
@@ -131,7 +131,7 @@ def get_archive_path(mass_path_root: str, var_dict: Dict[str, str], request: Req
     return mass_path_var
 
 
-def get_output_and_stored_files(invalid_var: dict) -> str:
+def log_archiving_issues(invalid_var: dict) -> str:
     """
     Generate an additional error message to the user if the mass status is 'UNKNOWN'. The error message contains the
     first and last 3 files that have been produced and are attempting to be stored, as well as the first and last 3
@@ -151,26 +151,29 @@ def get_output_and_stored_files(invalid_var: dict) -> str:
         relevant existing files already in storage along with their state and version.
     """
     nl = '\n'
-    mip_output_file_paths = sorted(invalid_var["mip_output_files"])
-    mip_output_files = [Path(file).stem for file in mip_output_file_paths]
-
+    mip_output = sorted(invalid_var["mip_output_files"])
+    output_parent_path = set(Path(file).parent for file in mip_output)
+    mip_output_files = [Path(file).stem for file in mip_output]
     mip_output_head_tail = f'{nl.join(mip_output_files[:3])}\n...\n{nl.join(mip_output_files[-3:])}'
 
-    err_msg = ('\nThis means that CDDS cannot conclude whether this data can be safely archived.')
-    err_msg += f'\n\nYou are attempting to store {len(mip_output_files)} files:\n{mip_output_head_tail}\n'
-
+    err_msg = ('\nCDDS does not believe that this data can be archived, either due to it overlapping existing data or '
+               'time gaps between this and existing data in MASS.\nYou are attempting to store '
+               f'{len(mip_output_files)} files from')
+    if len(output_parent_path) == 1:
+        err_msg += f':\n{str(next(iter(output_parent_path)))}...\n{mip_output_head_tail}\n'
+    else:
+        err_msg += f'multiple directories:\n{output_parent_path}...\n{mip_output_head_tail}\n'
+        
     stored_data = invalid_var["stored_data"]
-    for state, _ in stored_data.items():
-        state = state
-        for version, _ in stored_data[state].items():
-            version = version
-
-            stored_file_paths = sorted(stored_data[state][version])
-            stored_files = [Path(file).stem for file in stored_file_paths]
+    for state, state_entry in stored_data.items():
+        for state_entry, _ in stored_data[state].items():
+            stored = sorted(stored_data[state][state_entry])
+            stored_parent_path = set(Path(file).parent for file in stored)
+            stored_files = [Path(file).stem for file in stored]
             stored_head_tail = f'{nl.join(stored_files[:3])}\n...\n{nl.join(stored_files[-3:])}'
 
             err_msg += (f'\nThere are {len(stored_files)} existing files in storage in the {state} state under version '
-                        f'{version}:\n{stored_head_tail}\n')
+                        f'{state_entry}:\n{str(next(iter(stored_parent_path)))}...\n{stored_head_tail}\n')
 
     return err_msg
 
@@ -229,7 +232,7 @@ def check_stored_status(
         err_msg += '{description}'.format(
             **MASS_STATUS_DICT[invalid_var['mass_status']])
         if invalid_var['mass_status'] == 'UNKNOWN':
-            err_msg += get_output_and_stored_files(invalid_var)
+            err_msg += log_archiving_issues(invalid_var)
         logger.critical(err_msg)
 
     return valid_vars, invalid_vars
