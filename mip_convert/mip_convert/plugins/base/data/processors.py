0ca4f640eb6329e656791506476bb8689dfe480e
Lauren Boon <lauren.boon@metoffice.gov.uk>
2026-01-21 13:16:19 +0000

    cherry-pick #724: Add root depth processor (#771)
    * #724: Create root depth processor
    
    * #724: Update functional test with expected output

--- mip_convert/mip_convert/plugins/base/data/processors.py
+++ mip_convert/mip_convert/plugins/base/data/processors.py
@@ -2321,77 +2321,19 @@ def tos_ORCA12(tos_con, tossq_con):
     return tos_con
 
 
-def calc_slthick(soil_cube, frac_cube, ice_class=None):
-    """Returns a cube of soil layer thickness calculated from a cube on a model's standard latitude-longitude grid and
+def calc_rootd(soil_cube, frac_cube, ice_class=None):
+    """
+    Returns a cube of the maximum rooting depth calculated from a cube on a model's standard latitude-longitude grid and
     on soil levels.
 
     Parameters
     ----------
     soil_cube: :class:`iris.cube.Cube`
         A cube containing data on soil levels for that model.
+
     frac_cube: :class:`iris.cube.Cube`
         A cube containing JULES tile fractions for that model.
-    ice_class: str
-        The tile ID string for land ice. If present, the soil layer thickness is set to zero on land ice points.
 
-    Returns
-    -------
-    :class:`iris.cube.Cube`
-        A cube containing the thickness of each soil level of that model.
-
-    Raises
-    ------
-    RuntimeError
-        If the given soil cube does not contain cell bounds on the z axis or the soil cube is not masked.
-    ValueError
-        If the soil is identified as having a negative thickness.
-    """
-    # Calculate the thickness of a soil layer from the cell bounds of the vertical coord of the input cube.
-    depth_coord = _z_axis(soil_cube)
-    slthick_data = soil_cube.data.copy()
-    for cell, data in zip(depth_coord, slthick_data):
-        if not cell.has_bounds():
-            raise RuntimeError("The provided cube does not contian cell bounds on the identified Z axis.")
-        data[:] = cell.bounds[0][1] - cell.bounds[0][0]
-    if np.any(data < 0):
-        raise ValueError("Soil cannot have a negative thickness.")
-
-    # Set max root depth to 0.0 m on land ice points.
-    if ice_class:
-        ice_frac = frac_cube.extract(_pseudo_constraint(ice_class))
-        ice_cube = _collapse_pseudo(ice_frac, SUM)
-        ice_mask = ice_cube.data.data > 1e-6
-        slthick_data[:, ice_mask] = 0.0
-
-    # Copy the land/sea mask from the source cube to the new array.
-    if not is_masked(soil_cube.data):
-        raise RuntimeError("The soil cube is not masked, any land/sea masks will not be copied to the new array.")
-
-    slthick_data.mask = soil_cube.data.mask
-
-    # Create a Cube of the soil level thickness data.
-    dim_coords_and_dims = [(coord, k) for (k, coord) in enumerate(soil_cube.dim_coords)]
-
-    slthick_cube = iris.cube.Cube(slthick_data,
-                                  standard_name="cell_thickness",
-                                  long_name="Thickness of Soil Layers",
-                                  units=depth_coord.units,
-                                  dim_coords_and_dims=dim_coords_and_dims,
-                                  )
-
-    return slthick_cube
-
-
-def calc_rootd(soil_cube, frac_cube, ice_class=None):
-    """Returns a cube of the maximum rooting depth calculated from a cube on a model's standard latitude-longitude grid
-    and on soil levels.
-
-    Parameters
-    ----------
-    soil_cube: :class:`iris.cube.Cube`
-        A cube containing data on soil levels for that model.
-    frac_cube: :class:`iris.cube.Cube`
-        A cube containing JULES tile fractions for that model.
     ice_class: str
         Tile ID string for land ice.  If present, max root depth is set to zero on land ice points.
 
