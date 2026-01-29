2208bd03ab022c1b16dd93d9ba37a5bbf0f1bc38
mo-gill <edward.gillett@metoffice.gov.uk>
2026-01-29 09:46:29 +0000

    #768: Add flag to mip_convert to silence iris microseconds warnings.

--- mip_convert/mip_convert/__init__.py
+++ mip_convert/mip_convert/__init__.py
@@ -7,6 +7,11 @@ using |model output files| and information provided in the
 from mip_convert.versions import get_version
 from os import environ
 
+# Opt-in to microsecond precision to prevent FutureWarning appearing whenever
+# cubes are logged.
+import iris
+iris.FUTURE.date_microseconds = True
+
 # these need to be called before numpy imports
 environ["OMP_NUM_THREADS"] = "1"
 environ["OPENBLAS_NUM_THREADS"] = "1"
