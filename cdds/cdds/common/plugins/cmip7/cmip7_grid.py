547ebb7b1e48f6a02c3f36b4a34739539d65cac0
Matthew Mizielinski <matthew.mizielinski@metoffice.gov.uk>
2026-02-03 14:14:39 +0000

    #736: upgrade copyrights

--- cdds/cdds/common/plugins/cmip7/cmip7_grid.py
+++ cdds/cdds/common/plugins/cmip7/cmip7_grid.py
@@ -1,4 +1,4 @@
-# (C) British Crown Copyright 2021-2025, Met Office.
+# (C) British Crown Copyright 2021-2026, Met Office.
 # Please see LICENSE.md for license details.
 """The :mod:`cmip7_grid` module contains the code required
 handle grid information for CMIP7 models.
--- cdds/cdds/common/request/validations/cv_validators.py
+++ cdds/cdds/common/request/validations/cv_validators.py
@@ -1,4 +1,4 @@
-# (C) British Crown Copyright 2024-2025, Met Office.
+# (C) British Crown Copyright 2024-2026, Met Office.
 # Please see LICENSE.md for license details.
 """Module to validate request against the controlled vocabulary"""
 import os
