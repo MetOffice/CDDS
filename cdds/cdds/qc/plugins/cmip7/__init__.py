1fb3284bc0175102c3241852cba4cb404b6dc510
Jared Drayton <68954395+mo-jareddrayton@users.noreply.github.com>
2026-02-03 13:14:41 +0000

    #747 UserParentAttributes check working

--- cdds/cdds/qc/plugins/cmip7/__init__.py
+++ cdds/cdds/qc/plugins/cmip7/__init__.py
@@ -93,8 +93,8 @@ class CMIP7Check(BaseNCCheck):
             # StringAttributesCheckTask(self.__cache).relax_cmor(self.relaxed_cmor),
             # VariableAttributesCheckTask(self.__cache),
             # ComplexAttributesCheckTask(self.__cache),
-            OrphanAttributesCheckTask(self.__cache).relax_cmor(self.relaxed_cmor), # CMIP6
-            # UserParentAttributesCheckTask(self.__cache),
+            OrphanAttributesCheckTask(self.__cache).relax_cmor(self.relaxed_cmor),  # CMIP6
+            UserParentAttributesCheckTask(self.__cache),  # CMIP6
         ]
         if not self.relaxed_cmor:
             check_tasks.append(ParentConsistencyCheckTask(self.__cache))
