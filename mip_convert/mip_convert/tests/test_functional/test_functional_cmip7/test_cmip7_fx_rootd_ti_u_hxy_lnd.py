809e8dfc816bd25a10b679bbb33454aa3949caf3
mo-gill <edward.gillett@metoffice.gov.uk>
2026-02-17 16:29:17 +0000

    #802: Amend other CMIP7  functional tests so they pass.

--- mip_convert/mip_convert/tests/test_functional/test_functional_cmip7/test_cmip7_fx_rootd_ti_u_hxy_lnd.py
+++ mip_convert/mip_convert/tests/test_functional/test_functional_cmip7/test_cmip7_fx_rootd_ti_u_hxy_lnd.py
@@ -41,8 +41,8 @@ class TestCmip7_fx_rootd_ti_u_hxy_lnd(AbstractFunctionalTests):
                     'ancil': {'CMIP7_land@fx': 'rootd_ti-u-hxy-lnd'}
                 },
                 other={
-                    'reference_version': 'v2',
-                    'filenames': ['rootd_ti-u-hxy-lnd_fx_glb_gn_PCMDI-test-1-0_1pctCO2_r1i1dp1f1.nc'],
+                    'reference_version': 'v3',
+                    'filenames': ['rootd_ti-u-hxy-lnd_fx_glb_g100_UKESM1-3-LL_1pctCO2_r1i1p1f3.nc'],
                     'ignore_history': True,
                 }
             )
