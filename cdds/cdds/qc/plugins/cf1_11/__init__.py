6060c87213f5d676f39be51e52e56e21ae780eef
Jared Drayton <68954395+mo-jareddrayton@users.noreply.github.com>
2026-02-17 14:47:02 +0000

    #792 Add a cf1_11 plugin

---
+++ cdds/cdds/qc/plugins/cf1_11/__init__.py
@@ -0,0 +1,53 @@
+# (C) British Crown Copyright 2026, Met Office.
+# Please see LICENSE.md for license details.
+
+import re
+
+from compliance_checker.base import BaseCheck, Result
+from compliance_checker.cf.cf import CF1_11Check
+from netCDF4 import Dataset
+
+from cdds.qc.plugins.cf_mixin import CFMixin
+
+
+class CF111Check(CFMixin, CF1_11Check):
+    _cc_spec_version = "1.11"
+    _cc_spec = "cf111"
+    register_checker = True
+    name = "cf111"
+    supported_ds = [Dataset]
+
+    def check_conventions_version(self, ds):
+        """Check the global attribute conventions to contain CF-1.7
+        CF 2.6.1 the NUG defined global attribute Conventions to the string
+        value "CF-1.7" or "CMIP 6.2"
+
+        Parameters
+        ----------
+        ds : netCDF4.Dataset
+            An open netCDF dataset
+
+        Returns
+        -------
+        compliance_checker.base.Result
+            Result of the version validation
+        """
+
+        valid_conventions = ["CF-1.11", "CF-1.12", "CMIP-7.0"]
+        conventions = self.global_attributes_cache.getncattr("Conventions", ds, True)
+        if conventions is not None:
+            conventions = re.split(r",|\s+", conventions)
+            if any((c.strip() in valid_conventions for c in conventions)):
+                valid = True
+                reasoning = []
+            else:
+                valid = False
+                reasoning = ["Conventions global attribute does not contain 'CF-1.11'."]
+        else:
+            valid = False
+            reasoning = ["Conventions field is not present"]
+        return Result(
+            BaseCheck.MEDIUM, valid,
+            "2.6.1 Global Attribute Conventions includes CF-1.11",
+            msgs=reasoning
+        )
