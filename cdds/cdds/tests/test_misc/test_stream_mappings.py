4b928320699069b96694bc2e0b40e3cf454cd18f
mo-laurenboon <lauren.boon@metoffice.gov.uk>
2026-02-23 15:08:36 +0000

    #808: make tests pass

--- cdds/cdds/tests/test_misc/test_stream_mappings.py
+++ cdds/cdds/tests/test_misc/test_stream_mappings.py
@@ -3,43 +3,55 @@
 
 import unittest
 
-from unittest.mock import patch, mock_open
-
-from cdds.misc.stream_mappings import read_variables_file, check_mappings
+from cdds.misc.stream_mappings import read_variables_file, check_mappings, save_mappings
 
 
 class TestStreamMappings(unittest.TestCase):
 
     def test_read_variables_file(self):
-        result = read_variables_file("cdds/cdds/tests/test_misc/data/minimal_variables.txt")
-
         expected = {
+            ('day', 'tas'): (None, 'comment'),
             ('COMMENT', ' '): (' ', '#another comment\n'),
-            ('atmos', 'epfy_tavg-p39-hy-air@mon'): (None, ''),
-            ('atmos', 'tas_tavg-h2m-hxy-u@day'): ('ap6', 'comment'),
-            ('land', 'sftgif_ti-u-hxy-u@fx'): (None, '')
+            ('EmonZ', 'epfy'): (None, ''),
+            ('fx', 'sftgif'): (None, '')
         }
 
+        result = read_variables_file("cdds/cdds/tests/test_misc/data/inputs/minimal_variables.txt")
         msg = f"Error: variable dictionary not created as expected.\nExpected\n{expected}\ngot\n{result}"
         self.assertEqual(result, expected, msg)
 
     def test_check_mappings(self):
         variables = {
+            ('day', 'tas'): (None, 'comment'),
             ('COMMENT', ' '): (' ', '#another comment\n'),
-            ('atmos', 'epfy_tavg-p39-hy-air@mon'): (None, ''),
-            ('atmos', 'tas_tavg-h2m-hxy-u@day'): ('ap6', 'comment'),
-            ('land', 'sftgif_ti-u-hxy-u@fx'): (None, '')
+            ('EmonZ', 'epfy'): (None, ''),
+            ('fx', 'sftgif'): (None, '')
         }
-        result = check_mappings(variables)
-        # TEMP EXPECTED, INVESTIGATE WHY UNKNOWN
         expected = {
+            ('day', 'tas'): ('ap6', 'comment'),
             ('COMMENT', ' '): (' ', '#another comment\n'),
-            ('atmos', 'epfy_tavg-p39-hy-air@mon'): ('unknown', ''),
-            ('atmos', 'tas_tavg-h2m-hxy-u@day'): ('ap6', 'comment'),
-            ('land', 'sftgif_ti-u-hxy-u@fx'): ('unknown', '')
+            ('EmonZ', 'epfy'): ('ap5', ''),
+            ('fx', 'sftgif'): ('ancil', '')
         }
 
+        result = check_mappings(variables)
         msg = f"Error: variable dictionary not updated with streams as expected.\nExpected\n{expected}\ngot\n{result}"
+        self.assertEqual(result, expected, msg)
+
+    def test_save_mappings(self):
+        filepath = 'cdds/cdds/tests/test_misc/data/outputs/minimal_variables_final_output.txt'
+        variables = {
+            ('day', 'tas'): ('ap6', 'comment'),
+            ('COMMENT', ' '): (' ', '#another comment\n'),
+            ('EmonZ', 'epfy'): ('ap5', ''),
+            ('fx', 'sftgif'): ('ancil', '')
+        }
+        expected = 'day/tas:ap6 # comment\n#another comment\nEmonZ/epfy:ap5\nfx/sftgif:ancil\n'
+
+        save_mappings(filepath, variables)
+        with open(filepath, "r") as f:
+            result = f.read()
+        msg = f"Error: updated variable file not updated with streams as expected.\nExpected\n{expected}\ngot\n{result}"
         self.assertEqual(result, expected, msg)
 
 
