de6f7c756b002477ea9aca08c8ef687140ab59ff
mo-laurenboon <lauren.boon@metoffice.gov.uk>
2026-03-02 10:08:52 +0000

    #808: update with incoming changes

--- cdds/cdds/misc/stream_mappings.py
+++ cdds/cdds/misc/stream_mappings.py
@@ -63,7 +63,6 @@ def check_mappings(variables: dict) -> dict:
     load_plugin('CMIP6')
     plugin = PluginStore.instance().get_plugin()
     streams = plugin.stream_info()
-    breakpoint()
     for key, (stream, comment) in variables.items():
         if stream is None:
             streaminfo = streams.retrieve_stream_id(key[1], key[0])
