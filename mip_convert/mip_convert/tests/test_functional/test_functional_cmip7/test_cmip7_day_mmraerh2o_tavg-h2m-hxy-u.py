4b3cf8d6ae0b620bf69af288213c8d6d1a22e397
mo-gill <edward.gillett@metoffice.gov.uk>
2026-05-22 07:42:21 +0000

    #871: Add a functional test for mmraerh2o using new processor.

--- mip_convert/mip_convert/tests/test_functional/test_functional_cmip7/test_cmip7_day_mmraerh2o_tavg-h2m-hxy-u.py
+++ mip_convert/mip_convert/tests/test_functional/test_functional_cmip7/test_cmip7_day_mmraerh2o_tavg-h2m-hxy-u.py
@@ -20,13 +20,18 @@ class TestCmip7_mmraerh2o_tavg_h2m_hxy_u(AbstractFunctionalTests):
             variables=['mmraerh2o_tavg-h2m-hxy-u'],
             specific_info=SpecificInfo(
                 common={
-                    'test_location': test_location
-                },
+                    'test_location': test_location},
                 cmor_setup={
                     'cmor_log_file': get_cmor_log(test_location)
                 },
                 cmor_dataset={
-                    'output_dir': get_output_dir(test_location)
+                    'output_dir': get_output_dir(test_location),
+                    'calendar': 'standard',
+                    'branch_date_in_child': "1850-01-01T00:00:00",
+                    'branch_date_in_parent': "1850-01-01T00:00:00",
+                    'model_id': 'UKCM2-0-LL',
+                    'variant_label': 'r2i1p1f1',
+                    'parent_model_id': 'UKCM2-0-LL'
                 },
                 request={
                     'model_output_dir': MODEL_OUTPUT_DIR,
@@ -34,13 +39,16 @@ class TestCmip7_mmraerh2o_tavg_h2m_hxy_u(AbstractFunctionalTests):
                     'suite_id': 'u-dv623',
                     'mip_convert_plugin': 'UKCM2'
                 },
+                global_attributes={
+                    'further_info_url': 'https://furtherinfo.es-doc.org/CMIP7.MOHC.UKCM2-0-LL.1pctCO2.none.r2i1p1f1'
+                },
                 streams={
-                    'ap6': {'CMIP7_aerosol@mon': 'mmraerh2o_tavg-h2m-hxy-u'}
+                    'ap6': {'CMIP7_aerosol@day': 'mmraerh2o_tavg-h2m-hxy-u'}
                 },
                 other={
                     'reference_version': 'v1',
                         'filenames': [
-                            'mmraerh2o_tavg-h2m-hxy-u_day_glb_g100_UKCM2-0-LL_1pctCO2_r2i1p1f1_19000101-19000131.nc',
+                            'mmraerh2o_tavg-h2m-hxy-u_day_glb_g100_UKCM2-0-LL_1pctCO2_r2i1p1f1_19000101-19000131.nc'
                         ],
                     'ignore_history': True,
                 }
