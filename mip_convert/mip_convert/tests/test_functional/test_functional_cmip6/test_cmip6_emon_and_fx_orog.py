a207460bd3ef4a448ee5f79bc28873fcace2bd9b
Ed <146008263+mo-gill@users.noreply.github.com>
2026-06-02 14:20:38 +0100

    Issue #934: fix fx output dirs (#938)
    * #934: Rough attempt at fix for fx output dirs.
    
    * #934: Rough attempt at fx dir fix.
    
    * #934: Fix for oneliner causing fx/orog:afx failure.
    
    * #934: Remove auto generated tests as may not be needed.
    
    * #934: Refine some logic, adjust a comment.
    
    * #934: Add fix for side effect from fix in pp_filter.
    
    * #934: Add working func test for cmip6 Emon/orog:ap5.
    
    * #934: Refine func test and add test for fx_orog.
    
    * #934: Rename func test.
    
    * #934: Fix ruff failure.
    
    * #934: Minor adjustment to  wording in organise func.
    
    * #934: Refine function for readability.
    
    * #934: Further refinement of function.
    
    * #934: Add a unit test, adjust docstring.

---
+++ mip_convert/mip_convert/tests/test_functional/test_functional_cmip6/test_cmip6_emon_and_fx_orog.py
@@ -0,0 +1,55 @@
+# (C) British Crown Copyright 2022-2025, Met Office.
+# Please see LICENSE.md for license details.
+import os
+
+import pytest
+
+from mip_convert.tests.test_functional.test_command_line import AbstractFunctionalTests
+from mip_convert.tests.test_functional.utils.configurations import Cmip6TestData, SpecificInfo
+from mip_convert.tests.test_functional.utils.directories import (get_cmor_log, get_output_dir,
+                                                                 MODEL_OUTPUT_DIR,
+                                                                 ROOT_OUTPUT_CASES_DIR,
+                                                                 ROOT_ANCIL_DIR)
+
+
+class TestCmip6EmonOrog(AbstractFunctionalTests):
+
+    def get_test_data(self):
+        test_location = os.path.join(ROOT_OUTPUT_CASES_DIR, 'test_CMIP6_Emon_orog')
+        return Cmip6TestData(
+            mip_table='Emon',
+            variables=['orog'],
+            specific_info=SpecificInfo(
+                common={
+                    'test_location': test_location
+                },
+                cmor_setup={
+                    'cmor_log_file': get_cmor_log(test_location),
+                },
+                cmor_dataset={
+                    'output_dir': get_output_dir(test_location),
+                },
+                request={
+                    'ancil_files': os.path.join(ROOT_ANCIL_DIR, 'UKESM1-0-LL', 'qrparm.orog.pp'),
+                    'model_output_dir': MODEL_OUTPUT_DIR,
+                    'run_bounds': '2000-01-01T00:00:00 2000-02-01T00:00:00',
+                    'suite_id': 'u-aw310',
+                    'mip_convert_plugin': 'UKESM1',
+                    'base_date': '1850-01-01T00:00:00'
+                },
+                streams={
+                    'ap5': {'CMIP6_Emon': 'orog'},
+                    'afx': {'CMIP6_fx': 'orog'}
+                },
+                other={
+                    'reference_version': 'v1',
+                    'filenames': ['orog_Emon_UKESM1-0-LL_amip_r1i1p1f1_gn_200001-200001.nc',
+                                  'orog_fx_UKESM1-0-LL_amip_r1i1p1f1_gn.nc'],
+                    'ignore_history': True,
+                }
+            )
+        )
+
+    @pytest.mark.slow
+    def test_cmip6_emon_orog(self):
+        self.check_convert()
