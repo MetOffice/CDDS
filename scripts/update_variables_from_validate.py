08fafdaed6a4c80117f4095fe4479c485f0a1f8e
Lauren Boon <lauren.boon@metoffice.gov.uk>
2026-06-15 16:02:28 +0100

    #957: update copyright statement

--- scripts/update_variables_from_validate.py
+++ scripts/update_variables_from_validate.py
@@ -1,4 +1,4 @@
-# (C) British Crown Copyright 2025, Met Office.
+# (C) British Crown Copyright 2026, Met Office.
 # Please see LICENSE.md for license details.
 """
 This script removes variables from the variable list that have been identifies by extract validate as containing
