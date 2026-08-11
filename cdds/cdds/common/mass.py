fa74faf0d95133a961e52f8b8de48555de1ad3a4
mo-gill <edward.gillett@metoffice.gov.uk>
2026-08-11 10:52:41 +0000

    #1009: Fix retrieve mass_list_files_recursively.

--- cdds/cdds/common/mass.py
+++ cdds/cdds/common/mass.py
@@ -235,7 +235,7 @@ def mass_list_files_recursively(mass_path, simulation):
         if m[0] == 'F':
             elems = [s1 for s1 in m.split()]
             (mip, institution, model, experiment, variant, mip_table, variable, grid, status,
-             timestamp, filename) = elems[8].split('/')[-11:]
+             timestamp, filename) = elems[6].split('/')[-11:]
             if filename.endswith('.nc'):
                 dataset_id = '{}.{}.{}.{}.{}.{}.{}.{}.{}'.format(
                     'CMIP6', mip, institution, model, experiment, variant, mip_table, variable, grid)
@@ -246,9 +246,9 @@ def mass_list_files_recursively(mass_path, simulation):
                         'files': []
                     }
                 datasets[dataset_id]['files'].append({
-                    'filesize': elems[4],
+                    'filesize': elems[2],
                     'filename': filename,
-                    'mass_path': elems[8]
+                    'mass_path': elems[6]
                 })
     return datasets
 
