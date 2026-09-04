32e5d2e2163f1345f5f121e9d3f0320bdc803083
Ed <146008263+mo-gill@users.noreply.github.com>
2026-09-04 13:39:42 +0100

    #1086: fix cdds_clean (#1087)
    * #1086: Remove unnecessary cylc version check.
    
    * #1086: Remove unnecessary test.
    
    * #1086: Further fix.
    
    * #1086: Amend tests.
    
    * #1086: Further iteration, more logging, adjust tests.
    
    * #1086: Add a test.
    
    * #1086: Changed mind, remove test.
    
    * #1086: Amend docs.
    
    * #1086: Tidy up.
    
    * Update cdds/cdds/clean/workflows.py
    
    Co-authored-by: Matthew Mizielinski <matthew.mizielinski@metoffice.gov.uk>
    
    * #1086: Add prompt from user functionality.
    
    * #1086: adjust docs.
    
    * #1086: minor grammar change.
    
    * #1086: Reorder, enhance prompt.
    
    * #1086: Amend tests after change.
    
    * #1086: Adjust error output.
    
    * #1086: remove blank line.
    
    ---------
    
    Co-authored-by: Matthew Mizielinski <matthew.mizielinski@metoffice.gov.uk>

--- cdds/cdds/clean/workflows.py
+++ cdds/cdds/clean/workflows.py
@@ -1,60 +1,88 @@
 # (C) British Crown Copyright 2024-2025, Met Office.
 # Please see LICENSE.md for license details.
-"""Module to provide functionality for CDDS workflows"""
+"""Module to provide functionality for tearing down CDDS workflows"""
 import logging
+import shutil
 
 from cdds.common import run_command
 from cdds.common.request.request import Request
 
 
-def clean_workflows(request: Request) -> None:
-    """Clean CDDS streams workflows with the workflow base name containing the request
+def _confirm_teardown(data_dir: str, workflow_name: str) -> bool:
+    """Require explicit user confirmation before deleting data."""
+    response = input(
+        "This will permanently delete input and output data from '{}', and clean it's associated workflow '{}'. \n"
+        "Type 'yes' to continue: ".format(data_dir, workflow_name)
+    ).strip().lower()
+
+    return response == 'yes'
+
+
+def run_teardown(request: Request) -> None:
+    """Remove data directory and clean the CDDS workflow associated with the given request.
 
     Parameters
     ----------
     request : Request
-        Request containing information about the workflows
+        Request containing information about the workflow
     """
-    check_if_cylc_version()
-
-    workflow_name = 'cdds_{request_id}_{stream}'
-    for argument in request.conversion.cylc_args:
-        if argument.startswith("--workflow-name"):
-            # When loading the conversion section the `_{stream}` will already be added if
-            # the `--workflow-name` is already be set. So, no need here to add `_{stream}`.
-            workflow_name = argument.split("=")[1]
-
+    logger = logging.getLogger(__name__)
+    # First check workflow name wasn't used in cylc_args field.
+    # Possibly a hangover from an older version of cdds.
+    # Have switched to an error just in case someone tries to use it.
+    for argument in request.conversion.cylc_args or []:
+        # Catch both '--workflow-name' and '--workflow-name=<value>' forms.
+        if argument == '--workflow-name' or argument.startswith('--workflow-name'):
+            raise ValueError(
+                "'--workflow-name' detected in the request file's cylc_args, "
+                "please contact the CDDS team for guidance, or remove associated workflows and data by hand."
+            )
+    data_dir = request.common.root_data_dir
     request_id = request.common.workflow_basename
+    workflow_name = f'cdds_{request_id}'
 
-    for stream in request.data.streams:
-        stream_workflow = workflow_name.format(request_id=request_id, stream=stream)
-        clean_workflow(stream_workflow)
+    if not _confirm_teardown(data_dir, workflow_name):
+
+        logger.info("Teardown cancelled; no data was removed.")
+        return
+
+    clean_workflow(workflow_name)
+    remove_data_dir(data_dir)
+    logger.info('cdds_clean complete.')
+
+
+def remove_data_dir(data_dir: str) -> None:
+    """Remove the specified data directory.
+
+    Parameters
+    ----------
+    data_dir : str
+        Path to the data directory to be removed.
+    """
+    logger = logging.getLogger(__name__)
+    logger.info('Removing data directory: {}'.format(data_dir))
+
+    try:
+        shutil.rmtree(data_dir)
+    except OSError:
+        logger.exception('Failed to remove data directory: %s', data_dir)
+        raise
+
+    logger.info('Data directory removal complete.')
 
 
 def clean_workflow(workflow_name: str) -> None:
-    """Clean the workflow with the given name
+    """Clean the workflow with the given name.
 
     Parameters
     ----------
     workflow_name : str
-        Name of the workflow that should be cleaned
+        Name of the workflow that should be cleaned.
     """
+
     logger = logging.getLogger(__name__)
     logger.info('Clean workflow {}'.format(workflow_name))
 
     clean_command = ['cylc', 'clean', workflow_name]
     stdout = run_command(clean_command)
     logger.info(stdout)
-
-
-def check_if_cylc_version() -> None:
-    """Check that cylc >= 8 is active and raise an exception if not"""
-    logger = logging.getLogger(__name__)
-
-    cylc_version_command = ['cylc', '--version']
-    cylc_version = run_command(cylc_version_command)
-
-    if not cylc_version or not cylc_version.startswith('8'):
-        message = 'Cylc version >=8 must be active.'
-        logger.critical(message)
-        raise ValueError(message)
